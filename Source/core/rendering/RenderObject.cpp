9d68fc3eb9ab6e5b01fc2da05c7e921643382b79
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 19:54:14 +0000

    Merge 150271 "We don't care about layout root being set when doc..."
    BUG=238363
    TBR=inferno@chromium.org
    
    Review URL: https://codereview.chromium.org/15777009
    
    git-svn-id: http://src.chromium.org/blink/branches/chromium/1500@151290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/rendering/RenderObject.cpp
+++ Source/core/rendering/RenderObject.cpp
@@ -2370,7 +2370,8 @@ inline void RenderObject::clearLayoutRootIfNeeded() const
     if (!documentBeingDestroyed() && frame()) {
         if (FrameView* view = frame()->view()) {
             if (view->layoutRoot() == this) {
-                ASSERT_NOT_REACHED();
+                if (!documentBeingDestroyed())
+                    ASSERT_NOT_REACHED();
                 // This indicates a failure to layout the child, which is why
                 // the layout root is still set to |this|. Make sure to clear it
                 // since we are getting destroyed.
