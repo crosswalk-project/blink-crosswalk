b84478b843b62db1ecf9b6f5edfeffb1357f39eb
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 22:28:28 +0000

    Merge 151074 "Don't assert when serializing -mask property."
    BUG=242931
    TBR=alexis.menard@intel.com
    
    Review URL: https://codereview.chromium.org/15892011
    
    git-svn-id: http://src.chromium.org/blink/branches/chromium/1500@151318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/css/StylePropertySerializer.cpp
+++ Source/core/css/StylePropertySerializer.cpp
@@ -598,6 +598,12 @@ String StylePropertySerializer::getLayeredShorthandValue(const StylePropertyShor
                     else
                         yValue = nextValue;
 
+                    // background-repeat-x(y) or mask-repeat-x(y) may be like this : "initial, repeat". We can omit the implicit initial values
+                    // before starting to compare their values.
+                    if (value->isImplicitInitialValue() || yValue->isImplicitInitialValue())
+                        continue;
+
+
                     int xId = static_cast<CSSPrimitiveValue*>(value.get())->getIdent();
                     int yId = static_cast<CSSPrimitiveValue*>(yValue.get())->getIdent();
                     if (xId != yId) {
