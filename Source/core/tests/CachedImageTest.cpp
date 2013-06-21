benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 5cf7eba972ecf0739db1a75730d108d46b86889a

Rename CachedResource::data to appendData and simplify it.
The simplification is to be more consistent about what data type we pass
up the class hierarchy when data is received via ResourceLoader. Currently,
ResourceLoader converts the const char* and int into a ResourceBuffer, which
is passed to the CachedResource, which then converts back to const char* and
int if it sends that data to its CachedResourceClient.

Furthermore, we end up buffering the data in two different layers and needing
to track the data buffering policy in multiple places. This change will allow
ResourceLoader to just pass data through and not need to buffer anything.

BUG=none
TEST=none

Review URL: https://codereview.chromium.org/14139018

git-svn-id: svn://svn.chromium.org/blink/trunk@148560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/loader/cache/CachedImage.cpp
@@ -338,9 +338,9 @@ inline void CachedImage::clearImage()
     m_image.clear();
 }
 
-void CachedImage::data(PassRefPtr<ResourceBuffer> data)
+void CachedImage::appendData(const char* data, int length)
 {
-    CachedResource::data(data);
+    CachedResource::appendData(data, length);
     if (!m_loadingMultipartContent)
         updateImage(false);
 }
@@ -391,9 +391,11 @@ void CachedImage::error(CachedResource::Status status)
 
 void CachedImage::responseReceived(const ResourceResponse& response)
 {
-    if (!m_response.isNull())
+    if (m_loadingMultipartContent) {
+        if (m_data)
+            finishOnePart();
         clear();
-    if (response.isMultipart())
+    } else if (response.isMultipart())
         m_loadingMultipartContent = true;
     CachedResource::responseReceived(response);
 }

third_party/WebKit/Source/core/html/ImageDocument.cpp
@@ -128,16 +128,14 @@ static float pageZoomFactor(const Document* document)
     return frame ? frame->pageZoomFactor() : 1;
 }
 
-void ImageDocumentParser::appendBytes(DocumentWriter*, const char*, size_t)
+void ImageDocumentParser::appendBytes(DocumentWriter*, const char* data, size_t length)
 {
     Frame* frame = document()->frame();
     Settings* settings = frame->settings();
     if (!frame->loader()->client()->allowImage(!settings || settings->areImagesEnabled(), document()->url()))
         return;
 
-    CachedImage* cachedImage = document()->cachedImage();
-    cachedImage->data(frame->loader()->documentLoader()->mainResourceData());
-
+    document()->cachedImage()->appendData(data, length);
     document()->imageUpdated();
 }
 
@@ -145,16 +143,7 @@ void ImageDocumentParser::finish()
 {
     if (!isStopped() && document()->imageElement()) {
         CachedImage* cachedImage = document()->cachedImage();
-
-        // If this is a multipart image, make a copy of the current part, since the resource data
-        // will be overwritten by the next part.
-        if (document()->frame()->loader()->documentLoader()->isLoadingMultipartContent()) {
-            RefPtr<ResourceBuffer> data = document()->frame()->loader()->documentLoader()->mainResourceData()->copy();
-            cachedImage->data(data);
-        }
-
         cachedImage->finish();
-
         cachedImage->setResponse(document()->frame()->loader()->documentLoader()->response());
 
         // Report the natural image size in the page title, regardless of zoom level.

third_party/WebKit/Source/core/loader/ResourceLoader.cpp
@@ -42,11 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryCache.h"
 #include "Page.h"
 #include "ProgressTracker.h"
-#include "ResourceBuffer.h"
 #include "ResourceError.h"
 #include "ResourceHandle.h"
 #include "SecurityOrigin.h"
-#include "SharedBuffer.h"
 
 namespace WebCore {
 
@@ -60,11 +58,6 @@ ResourceLoader::RequestCountTracker::RequestCountTracker(CachedResourceLoader* c
 ResourceLoader::RequestCountTracker::~RequestCountTracker()
 {
     m_cachedResourceLoader->decrementRequestCount(m_resource);
-}
-
-PassRefPtr<ResourceBuffer> ResourceLoader::resourceData()
-{
-    return m_resourceData;
 }
 
 PassRefPtr<ResourceLoader> ResourceLoader::create(Frame* frame, CachedResource* resource, const ResourceRequest& request, const ResourceLoaderOptions& options)
@@ -135,7 +128,6 @@ void ResourceLoader::releaseResources()
         m_handle = 0;
     }
 
-    m_resourceData = 0;
     m_deferredRequest = ResourceRequest();
 }
 
@@ -216,33 +208,6 @@ FrameLoader* ResourceLoader::frameLoader() const
     if (!m_frame)
         return 0;
     return m_frame->loader();
-}
-
-void ResourceLoader::setDataBufferingPolicy(DataBufferingPolicy dataBufferingPolicy)
-{ 
-    m_options.dataBufferingPolicy = dataBufferingPolicy; 
-
-    // Reset any already buffered data
-    if (dataBufferingPolicy == DoNotBufferData)
-        m_resourceData = 0;
-}
-    
-
-void ResourceLoader::addData(const char* data, int length)
-{
-    if (m_options.dataBufferingPolicy == DoNotBufferData)
-        return;
-        
-    if (m_resourceData)
-        m_resourceData->append(data, length);
-    else
-        m_resourceData = ResourceBuffer::create(data, length);
-}
-
-void ResourceLoader::clearResourceData()
-{
-    if (m_resourceData)
-        m_resourceData->clear();
 }
 
 void ResourceLoader::didDownloadData(ResourceHandle*, int length)
@@ -462,13 +427,7 @@ void ResourceLoader::didReceiveResponse(ResourceHandle*, const ResourceResponse&
             cancel();
             return;
         }
-    }
-
-    RefPtr<ResourceBuffer> buffer = resourceData();
-    if (m_loadingMultipartContent && buffer && buffer->size()) {
-        sendDataToResource(buffer->data(), buffer->size());
-        m_resource->finishOnePart();
-        clearResourceData();
+    } else if (m_loadingMultipartContent) {
         // Since a subresource loader does not load multipart sections progressively, data was delivered to the loader all at once.
         // After the first multipart section is complete, signal to delegates that this load is "finished"
         m_documentLoader->subresourceLoaderFinishedLoadingOnePart(this);
@@ -496,34 +455,15 @@ void ResourceLoader::didReceiveData(ResourceHandle*, const char* data, int lengt
     // anything including removing the last reference to this object.
     RefPtr<ResourceLoader> protect(this);
 
-    addData(data, length);
-
     // FIXME: If we get a resource with more than 2B bytes, this code won't do the right thing.
     // However, with today's computers and networking speeds, this won't happen in practice.
     // Could be an issue with a giant local file.
     if (m_options.sendLoadCallbacks == SendCallbacks && m_frame)
         frameLoader()->notifier()->didReceiveData(this, data, length, static_cast<int>(encodedDataLength));
 
-    if (!m_loadingMultipartContent)
-        sendDataToResource(data, length);
+    m_resource->appendData(data, length);
 
     InspectorInstrumentation::didReceiveResourceData(cookie);
-}
-
-void ResourceLoader::sendDataToResource(const char* data, int length)
-{
-    // There are two cases where we might need to create our own SharedBuffer instead of copying the one in ResourceLoader.
-    // (1) Multipart content: The loader delivers the data in a multipart section all at once, then sends eof.
-    //     The resource data will change as the next part is loaded, so we need to make a copy.
-    // (2) Our client requested that the data not be buffered at the ResourceLoader level via ResourceLoaderOptions. In this case,
-    //     ResourceLoader::resourceData() will be null. However, unlike the multipart case, we don't want to tell the CachedResource
-    //     that all data has been received yet.
-    RefPtr<ResourceBuffer> buffer;
-    if (m_loadingMultipartContent || !resourceData())
-        buffer = ResourceBuffer::create(data, length);
-    else
-        buffer = resourceData();
-     m_resource->data(buffer.release());
 }
 
 void ResourceLoader::didFinishLoading(ResourceHandle*, double finishTime)
@@ -599,7 +539,6 @@ void ResourceLoader::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
     info.addMember(m_documentLoader, "documentLoader");
     info.addMember(m_request, "request");
     info.addMember(m_originalRequest, "originalRequest");
-    info.addMember(m_resourceData, "resourceData");
     info.addMember(m_deferredRequest, "deferredRequest");
     info.addMember(m_options, "options");
     info.addMember(m_resource, "resource");

third_party/WebKit/Source/core/loader/ResourceLoader.h
@@ -74,9 +74,6 @@ public:
 
     void releaseResources();
 
-    PassRefPtr<ResourceBuffer> resourceData();
-    void clearResourceData();
-
     void didChangePriority(ResourceLoadPriority);
 
     // ResourceHandleClient
@@ -100,8 +97,6 @@ public:
 
     const ResourceRequest& request() const { return m_request; }
 
-    void setDataBufferingPolicy(DataBufferingPolicy);
-
     void reportMemoryUsage(MemoryObjectInfo*) const;
 
 private:
@@ -114,18 +109,13 @@ private:
 
     bool cancelled() const { return m_cancelled; }
 
-    void sendDataToResource(const char*, int);
-
     RefPtr<ResourceHandle> m_handle;
     RefPtr<Frame> m_frame;
     RefPtr<DocumentLoader> m_documentLoader;
     ResourceResponse m_response;
 
-    void addData(const char*, int);
-
     ResourceRequest m_request;
     ResourceRequest m_originalRequest; // Before redirects.
-    RefPtr<ResourceBuffer> m_resourceData;
     
     unsigned long m_identifier;
 

third_party/WebKit/Source/core/loader/cache/CachedImage.h
@@ -77,7 +77,7 @@ public:
     virtual void allClientsRemoved();
     virtual void destroyDecodedData();
 
-    virtual void data(PassRefPtr<ResourceBuffer> data) OVERRIDE;
+    virtual void appendData(const char*, int) OVERRIDE;
     virtual void error(CachedResource::Status);
     virtual void responseReceived(const ResourceResponse&);
     virtual void finishOnePart() OVERRIDE;

third_party/WebKit/Source/core/loader/cache/CachedRawResource.cpp
@@ -43,36 +43,14 @@ CachedRawResource::CachedRawResource(ResourceRequest& resourceRequest, Type type
 {
 }
 
-void CachedRawResource::data(PassRefPtr<ResourceBuffer> data)
+void CachedRawResource::appendData(const char* data, int length)
 {
+    CachedResource::appendData(data, length);
+
     CachedResourceHandle<CachedRawResource> protect(this);
-    const char* incrementalData = 0;
-    size_t incrementalDataLength = 0;
-    if (data) {
-        // If we are buffering data, then we are saving the buffer in m_data and need to manually
-        // calculate the incremental data. If we are not buffering, then m_data will be null and
-        // the buffer contains only the incremental data.
-        size_t previousDataLength = (m_options.dataBufferingPolicy == BufferData) ? encodedSize() : 0;
-        ASSERT(data->size() >= previousDataLength);
-        incrementalData = data->data() + previousDataLength;
-        incrementalDataLength = data->size() - previousDataLength;
-    }
-
-    if (m_options.dataBufferingPolicy == BufferData)
-        CachedResource::data(data);
-
-    DataBufferingPolicy dataBufferingPolicy = m_options.dataBufferingPolicy;
-    if (incrementalDataLength) {
-        CachedResourceClientWalker<CachedRawResourceClient> w(m_clients);
-        while (CachedRawResourceClient* c = w.next())
-            c->dataReceived(this, incrementalData, incrementalDataLength);
-    }
-
-    if (dataBufferingPolicy == BufferData && m_options.dataBufferingPolicy == DoNotBufferData) {
-        if (m_loader)
-            m_loader->setDataBufferingPolicy(DoNotBufferData);
-        clear();
-    }
+    CachedResourceClientWalker<CachedRawResourceClient> w(m_clients);
+    while (CachedRawResourceClient* c = w.next())
+        c->dataReceived(this, data, length);
 }
 
 void CachedRawResource::didAddClient(CachedResourceClient* c)
@@ -166,6 +144,7 @@ void CachedRawResource::setDefersLoading(bool defers)
 void CachedRawResource::setDataBufferingPolicy(DataBufferingPolicy dataBufferingPolicy)
 {
     m_options.dataBufferingPolicy = dataBufferingPolicy;
+    clear();
 }
 
 static bool shouldIgnoreHeaderForCacheReuse(AtomicString headerName)
@@ -233,8 +212,6 @@ void CachedRawResource::clear()
 {
     m_data.clear();
     setEncodedSize(0);
-    if (m_loader)
-        m_loader->clearResourceData();
 }
 
 void CachedRawResource::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const

third_party/WebKit/Source/core/loader/cache/CachedRawResource.h
@@ -53,7 +53,7 @@ public:
 
 private:
     virtual void didAddClient(CachedResourceClient*);
-    virtual void data(PassRefPtr<ResourceBuffer> data) OVERRIDE;
+    virtual void appendData(const char*, int) OVERRIDE;
 
     virtual bool shouldIgnoreHTTPStatusCodeErrors() const { return true; }
     virtual void allClientsRemoved();

third_party/WebKit/Source/core/loader/cache/CachedResource.cpp
@@ -350,10 +350,15 @@ void CachedResource::checkNotify()
         c->notifyFinished(this);
 }
 
-void CachedResource::data(PassRefPtr<ResourceBuffer> data)
+void CachedResource::appendData(const char* data, int length)
 {
-    m_data = data;
-    setEncodedSize(m_data ? m_data->size() : 0);
+    if (m_options.dataBufferingPolicy == DoNotBufferData)
+        return;
+    if (m_data)
+        m_data->append(data, length);
+    else
+        m_data = ResourceBuffer::create(data, length);
+    setEncodedSize(m_data->size());
 }
 
 void CachedResource::error(CachedResource::Status status)

crosswalk-project/blink-crosswalk

commit d6acac00596ae6907645acf5e5fa65330fe2b8c3

Merge 149966 "Fix multipart images, regressed in r148560".
BUG=238427
TEST=Manually on http://mjpeg.sanford.io/, CachedImageTest::MultipartImage
TBR=abarth

Review URL: https://codereview.chromium.org/14569024

git-svn-id: http://src.chromium.org/blink/branches/chromium/1500@150348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/core.gypi
@@ -3653,6 +3653,7 @@
             'platform/chromium/support/WrappedResourceResponse.h',
         ],
         'core_unittest_files': [
+            'tests/CachedImageTest.cpp',
             'tests/HeapGraphSerializerTest.cpp',
             'tests/LayoutUnit.cpp',
             'tests/LinkRelAttribute.cpp',

Source/core/loader/cache/CachedImage.cpp
@@ -377,7 +377,11 @@ void CachedImage::updateImage(bool allDataReceived)
 
 void CachedImage::finishOnePart()
 {
+    if (m_loadingMultipartContent)
+        clear();
     updateImage(true);
+    if (m_loadingMultipartContent)
+        m_data.clear();
     CachedResource::finishOnePart();
 }
 
@@ -390,11 +394,9 @@ void CachedImage::error(CachedResource::Status status)
 
 void CachedImage::responseReceived(const ResourceResponse& response)
 {
-    if (m_loadingMultipartContent) {
-        if (m_data)
-            finishOnePart();
-        clear();
-    } else if (response.isMultipart())
+    if (m_loadingMultipartContent && m_data)
+        finishOnePart();
+    else if (response.isMultipart())
         m_loadingMultipartContent = true;
     CachedResource::responseReceived(response);
 }

LayoutTests/http/tests/multipart/invalid-image-data.html
@@ -7,7 +7,7 @@
         function loadedFirstPart()
         {
             if (window.testRunner)
-                setTimeout("testRunner.notifyDone()", 200);
+                testRunner.notifyDone();
         }
     </script>
 </head>
@@ -19,6 +19,6 @@
     <p>
         There should be a bright green square below.
     </p>
-    <img width="100" height="100" src="resources/multipart.php?interval=0&img1=2x2-green.png&img2=abe.png" onload="loadedFirstPart()">
+    <img width="100" height="100" src="resources/multipart.php?interval=1&img1=2x2-green.png&img2=abe.png" onload="loadedFirstPart()">
 </body>
 </html>

Source/core/tests/CachedImageTest.cpp
@@ -1,0 +1,115 @@
+/*
+ * Copyright (c) 2013, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "core/loader/cache/CachedImage.h"
+
+#include "core/loader/cache/CachedImageClient.h"
+#include "core/loader/cache/CachedResourceHandle.h"
+#include "core/platform/SharedBuffer.h"
+#include <gtest/gtest.h>
+
+using namespace WebCore;
+
+namespace {
+
+class MockCachedImageClient : public WebCore::CachedImageClient {
+public:
+    MockCachedImageClient()
+        : m_imageChangedCount(0)
+        , m_notifyFinishedCalled(false)
+    {
+    }
+
+    virtual ~MockCachedImageClient() { }
+    virtual void imageChanged(CachedImage*, const IntRect*)
+    {
+        m_imageChangedCount++;
+    }
+
+    virtual void notifyFinished(CachedResource*)
+    {
+        ASSERT_FALSE(m_notifyFinishedCalled);
+        m_notifyFinishedCalled = true;
+    }
+
+    int imageChangedCount() const { return m_imageChangedCount; }
+    bool notifyFinishedCalled() const { return m_notifyFinishedCalled; }
+
+private:
+    int m_imageChangedCount;
+    bool m_notifyFinishedCalled;
+};
+
+TEST(CachedImageTest, MultipartImage)
+{
+    CachedResourceHandle<CachedImage> cachedImage = new CachedImage(ResourceRequest());
+    cachedImage->setLoading(true);
+
+    MockCachedImageClient client;
+    cachedImage->addClient(&client);
+
+    // Send the multipart response. No image or data buffer is created.
+    cachedImage->responseReceived(ResourceResponse(KURL(), "multipart/x-mixed-replace", 0, String(), String()));
+    ASSERT_FALSE(cachedImage->resourceBuffer());
+    ASSERT_FALSE(cachedImage->hasImage());
+    ASSERT_EQ(client.imageChangedCount(), 0);
+    ASSERT_FALSE(client.notifyFinishedCalled());
+
+    // Send the response for the first real part. No image or data buffer is created.
+    const char* svgData = "<svg xmlns='http://www.w3.org/2000/svg' width='1' height='1'><rect width='1' height='1' fill='green'/></svg>";
+    unsigned svgDataLength = strlen(svgData);
+    cachedImage->responseReceived(ResourceResponse(KURL(), "image/svg+xml", svgDataLength, String(), String()));
+    ASSERT_FALSE(cachedImage->resourceBuffer());
+    ASSERT_FALSE(cachedImage->hasImage());
+    ASSERT_EQ(client.imageChangedCount(), 0);
+    ASSERT_FALSE(client.notifyFinishedCalled());
+
+    // The first bytes arrive. The data buffer is created, but no image is created.
+    cachedImage->appendData(svgData, svgDataLength);
+    ASSERT_TRUE(cachedImage->resourceBuffer());
+    ASSERT_EQ(cachedImage->resourceBuffer()->size(), svgDataLength);
+    ASSERT_FALSE(cachedImage->hasImage());
+    ASSERT_EQ(client.imageChangedCount(), 0);
+    ASSERT_FALSE(client.notifyFinishedCalled());
+
+    // This part finishes. The image is created, callbacks are sent, and the data buffer is cleared.
+    cachedImage->finish();
+    ASSERT_FALSE(cachedImage->resourceBuffer());
+    ASSERT_FALSE(cachedImage->errorOccurred());
+    ASSERT_TRUE(cachedImage->hasImage());
+    ASSERT_FALSE(cachedImage->image()->isNull());
+    ASSERT_EQ(cachedImage->image()->width(), 1);
+    ASSERT_EQ(cachedImage->image()->height(), 1);
+    ASSERT_EQ(client.imageChangedCount(), 2);
+    ASSERT_TRUE(client.notifyFinishedCalled());
+}
+
+} // namespace