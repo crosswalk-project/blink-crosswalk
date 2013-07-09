78b32079971c2a722866e88ed7da2c25b36a37f1
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-09 05:50:54 +0000

    Merge 153625 "DevTools: Fix the way extremely long script conten..."
    > DevTools: Fix the way extremely long script content is loaded on page reload.
    > 
    > BUG=255592
    > 
    > Review URL: https://chromiumcodereview.appspot.com/18316005
    
    TBR=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/18534007
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1547@153752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/testing/Internals.cpp
+++ Source/core/testing/Internals.cpp
@@ -1399,6 +1399,16 @@ void Internals::closeDummyInspectorFrontend()
     m_frontendWindow.release();
 }
 
+Vector<unsigned long> Internals::setMemoryCacheCapacities(unsigned long minDeadBytes, unsigned long maxDeadBytes, unsigned long totalBytes)
+{
+    Vector<unsigned long> result;
+    result.append(memoryCache()->minDeadCapacity());
+    result.append(memoryCache()->maxDeadCapacity());
+    result.append(memoryCache()->capacity());
+    memoryCache()->setCapacities(minDeadBytes, maxDeadBytes, totalBytes);
+    return result;
+}
+
 void Internals::setInspectorResourcesDataSizeLimits(int maximumResourcesContentSize, int maximumSingleResourceContentSize, ExceptionCode& ec)
 {
     Page* page = contextDocument()->frame()->page();
