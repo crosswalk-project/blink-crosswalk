nwjs/blink

commit 794dffb55716208845cc7c217ab6c1dfc26cb617

Move subresource archive loading out of ResourceLoader
BUG=none

Review URL: https://chromiumcodereview.appspot.com/15697017

git-svn-id: svn://svn.chromium.org/blink/trunk@151344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/DocumentLoader.cpp
@@ -103,7 +103,6 @@ DocumentLoader::DocumentLoader(const ResourceRequest& req, const SubstituteData&
     , m_isClientRedirect(false)
     , m_isLoadingMultipartContent(false)
     , m_wasOnloadHandled(false)
-    , m_substituteResourceDeliveryTimer(this, &DocumentLoader::substituteResourceDeliveryTimerFired)
     , m_loadingMainResource(false)
     , m_timeOfLastDataReceived(0.0)
     , m_identifierForLoadWithoutResourceLoader(0)
@@ -690,8 +689,6 @@ void DocumentLoader::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
     info.addMember(m_originalRequestCopy, "originalRequestCopy");
     info.addMember(m_request, "request");
     info.addMember(m_response, "response");
-    info.addMember(m_pendingSubstituteResources, "pendingSubstituteResources");
-    info.addMember(m_substituteResourceDeliveryTimer, "substituteResourceDeliveryTimer");
     info.addMember(m_archiveResourceCollection, "archiveResourceCollection");
     info.addMember(m_archive, "archive");
     info.addMember(m_resourcesClientKnowsAbout, "resourcesClientKnowsAbout");
@@ -839,96 +836,23 @@ void DocumentLoader::prepareSubframeArchiveLoadIfNeeded()
 void DocumentLoader::clearArchiveResources()
 {
     m_archiveResourceCollection.clear();
-    m_substituteResourceDeliveryTimer.stop();
 }
 
-ArchiveResource* DocumentLoader::archiveResourceForURL(const KURL& url) const
+bool DocumentLoader::scheduleArchiveLoad(CachedResource* cachedResource, const ResourceRequest& request)
 {
-    if (!m_archiveResourceCollection)
-        return 0;
-        
-    ArchiveResource* resource = m_archiveResourceCollection->archiveResourceForURL(url);
+    if (!m_archive)
+        return false;
 
-    return resource && !resource->shouldIgnoreWhenUnarchiving() ? resource : 0;
-}
+    ASSERT(m_archiveResourceCollection);
+    ArchiveResource* archiveResource = m_archiveResourceCollection->archiveResourceForURL(request.url());
+    ASSERT(archiveResource);
 
-void DocumentLoader::deliverSubstituteResourcesAfterDelay()
-{
-    if (m_pendingSubstituteResources.isEmpty())
-        return;
-    ASSERT(m_frame && m_frame->page());
-    if (m_frame->page()->defersLoading())
-        return;
-    if (!m_substituteResourceDeliveryTimer.isActive())
-        m_substituteResourceDeliveryTimer.startOneShot(0);
-}
-
-void DocumentLoader::substituteResourceDeliveryTimerFired(Timer<DocumentLoader>*)
-{
-    if (m_pendingSubstituteResources.isEmpty())
-        return;
-    ASSERT(m_frame && m_frame->page());
-    if (m_frame->page()->defersLoading())
-        return;
-
-    SubstituteResourceMap copy;
-    copy.swap(m_pendingSubstituteResources);
-
-    SubstituteResourceMap::const_iterator end = copy.end();
-    for (SubstituteResourceMap::const_iterator it = copy.begin(); it != end; ++it) {
-        RefPtr<ResourceLoader> loader = it->key;
-        SubstituteResource* resource = it->value.get();
-        
-        if (resource) {
-            SharedBuffer* data = resource->data();
-        
-            loader->didReceiveResponse(0, resource->response());
-
-            // Calling ResourceLoader::didReceiveResponse can end up cancelling the load,
-            // so we need to check if the loader has reached its terminal state.
-            if (loader->reachedTerminalState())
-                return;
-
-            loader->didReceiveData(0, data->data(), data->size(), data->size());
-
-            // Calling ResourceLoader::didReceiveData can end up cancelling the load,
-            // so we need to check if the loader has reached its terminal state.
-            if (loader->reachedTerminalState())
-                return;
-
-            loader->didFinishLoading(0, 0);
-        } else {
-            // A null resource means that we should fail the load.
-            // FIXME: Maybe we should use another error here - something like "not in cache".
-            loader->didFail(0, loader->cannotShowURLError());
-        }
-    }
-}
-
-#ifndef NDEBUG
-bool DocumentLoader::isSubstituteLoadPending(ResourceLoader* loader) const
-{
-    return m_pendingSubstituteResources.contains(loader);
-}
-#endif
-
-void DocumentLoader::cancelPendingSubstituteLoad(ResourceLoader* loader)
-{
-    if (m_pendingSubstituteResources.isEmpty())
-        return;
-    m_pendingSubstituteResources.remove(loader);
-    if (m_pendingSubstituteResources.isEmpty())
-        m_substituteResourceDeliveryTimer.stop();
-}
-
-bool DocumentLoader::scheduleArchiveLoad(ResourceLoader* loader, const ResourceRequest& request)
-{
-    if (ArchiveResource* resource = archiveResourceForURL(request.url())) {
-        m_pendingSubstituteResources.set(loader, resource);
-        deliverSubstituteResourcesAfterDelay();
-        return true;
-    }
-    return m_archive;
+    cachedResource->setLoading(true);
+    SharedBuffer* data = archiveResource->data();
+    cachedResource->responseReceived(archiveResource->response());
+    cachedResource->appendData(data->data(), data->size());
+    cachedResource->finish();
+    return true;
 }
 
 void DocumentLoader::setTitle(const StringWithDirection& title)
@@ -990,8 +914,6 @@ void DocumentLoader::setDefersLoading(bool defers)
         mainResourceLoader()->setDefersLoading(defers);
 
     setAllDefersLoading(m_resourceLoaders, defers);
-    if (!defers)
-        deliverSubstituteResourcesAfterDelay();
 }
 
 void DocumentLoader::setMainResourceDataBufferingPolicy(DataBufferingPolicy dataBufferingPolicy)

Source/core/loader/DocumentLoader.h
@@ -121,11 +121,7 @@ namespace WebCore {
         void setTitle(const StringWithDirection&);
         const String& overrideEncoding() const { return m_overrideEncoding; }
 
-        bool scheduleArchiveLoad(ResourceLoader*, const ResourceRequest&);
-
-#ifndef NDEBUG
-        bool isSubstituteLoadPending(ResourceLoader*) const;
-#endif
+        bool scheduleArchiveLoad(CachedResource*, const ResourceRequest&);
         void cancelPendingSubstituteLoad(ResourceLoader*);
 
         bool shouldContinueForNavigationPolicy(const ResourceRequest&);
@@ -205,7 +201,6 @@ namespace WebCore {
 
         void prepareSubframeArchiveLoadIfNeeded();
         void addAllArchiveResources(MHTMLArchive*);
-        ArchiveResource* archiveResourceForURL(const KURL&) const;
 
         void willSendRequest(ResourceRequest&, const ResourceResponse&);
         void finishedLoading(double finishTime);
@@ -229,9 +224,6 @@ namespace WebCore {
         void handleSubstituteDataLoadSoon();
         void handleSubstituteDataLoadNow(DocumentLoaderTimer*);
         void startDataLoadTimer();
-
-        void deliverSubstituteResourcesAfterDelay();
-        void substituteResourceDeliveryTimerFired(Timer<DocumentLoader>*);
                 
         Frame* m_frame;
         RefPtr<CachedResourceLoader> m_cachedResourceLoader;
@@ -281,10 +273,6 @@ namespace WebCore {
         // The action that triggered loading - we keep this around for the
         // benefit of the various policy handlers.
         NavigationAction m_triggeringAction;
-        
-        typedef HashMap<RefPtr<ResourceLoader>, RefPtr<SubstituteResource> > SubstituteResourceMap;
-        SubstituteResourceMap m_pendingSubstituteResources;
-        Timer<DocumentLoader> m_substituteResourceDeliveryTimer;
 
         OwnPtr<ArchiveResourceCollection> m_archiveResourceCollection;
         RefPtr<MHTMLArchive> m_archive;

Source/core/loader/ResourceLoader.cpp
@@ -42,7 +42,6 @@
 #include "core/platform/Logging.h"
 #include "core/platform/network/ResourceError.h"
 #include "core/platform/network/ResourceHandle.h"
-#include "weborigin/SecurityOrigin.h"
 
 namespace WebCore {
 
@@ -129,7 +128,6 @@ bool ResourceLoader::init(const ResourceRequest& r)
     ASSERT(!m_handle);
     ASSERT(m_request.isNull());
     ASSERT(m_deferredRequest.isNull());
-    ASSERT(!m_documentLoader->isSubstituteLoadPending(this));
     
     ResourceRequest clientRequest(r);
 
@@ -152,9 +150,6 @@ void ResourceLoader::start()
     ASSERT(!m_handle);
     ASSERT(!m_request.isNull());
     ASSERT(m_deferredRequest.isNull());
-
-    if (m_documentLoader->scheduleArchiveLoad(this, m_request))
-        return;
 
     m_documentLoader->applicationCacheHost()->willStartLoadingResource(m_request);
 
@@ -247,7 +242,6 @@ void ResourceLoader::cancel(const ResourceError& error)
         m_state = Finishing;
     m_resource->setResourceError(nonNullError);
 
-    m_documentLoader->cancelPendingSubstituteLoad(this);
     if (m_handle) {
         m_handle->cancel();
         m_handle = 0;

Source/core/loader/archive/ArchiveResource.cpp
@@ -38,7 +38,6 @@ inline ArchiveResource::ArchiveResource(PassRefPtr<SharedBuffer> data, const KUR
     , m_mimeType(mimeType)
     , m_textEncoding(textEncoding)
     , m_frameName(frameName)
-    , m_shouldIgnoreWhenUnarchiving(false)
 {
 }
 

Source/core/loader/archive/ArchiveResource.h
@@ -44,17 +44,12 @@ public:
     const String& textEncoding() const { return m_textEncoding; }
     const String& frameName() const { return m_frameName; }
 
-    void ignoreWhenUnarchiving() { m_shouldIgnoreWhenUnarchiving = true; }
-    bool shouldIgnoreWhenUnarchiving() const { return m_shouldIgnoreWhenUnarchiving; }
-
 private:
     ArchiveResource(PassRefPtr<SharedBuffer>, const KURL&, const String& mimeType, const String& textEncoding, const String& frameName, const ResourceResponse&);
 
     String m_mimeType;
     String m_textEncoding;
     String m_frameName;
-
-    bool m_shouldIgnoreWhenUnarchiving;
 };
 
 }

Source/core/loader/cache/CachedResourceLoader.cpp
@@ -399,7 +399,8 @@ CachedResourceHandle<CachedResource> CachedResourceLoader::requestResource(Cache
         if (request.options().securityCheck == DoSecurityCheck && (frameLoader->state() == FrameStateProvisional || !frameLoader->activeDocumentLoader() || frameLoader->activeDocumentLoader()->isStopping()))
             return 0;
 
-        resource->load(this, request.options());
+        if (!m_documentLoader->scheduleArchiveLoad(resource.get(), request.resourceRequest()))
+            resource->load(this, request.options());
 
         // We don't support immediate loads, but we do support immediate failure.
         if (resource->errorOccurred()) {

crosswalk-project/blink-crosswalk

commit 10e4db1dd8cc82fa485092551614c1bb7beeed2d

Merge 153689 "Fix crash when a script request a resource missing..."
> Fix crash when a script request a resource missing from an mhtml archive.
> 
> This was probably regressed in r151344.
> 
> BUG=247091
> TEST=mhtml/new-image-not-in-archive.mht
> R=abarth@chromium.org
> 
> Review URL: https://codereview.chromium.org/17206004

TBR=japhet@chromium.org

Review URL: https://codereview.chromium.org/19017008

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1547@153935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/DocumentLoader.cpp
@@ -820,12 +820,16 @@ bool DocumentLoader::scheduleArchiveLoad(CachedResource* cachedResource, const R
 
     ASSERT(m_archiveResourceCollection);
     ArchiveResource* archiveResource = m_archiveResourceCollection->archiveResourceForURL(request.url());
-    ASSERT(archiveResource);
+    if (!archiveResource) {
+        cachedResource->error(CachedResource::LoadError);
+        return true;
+    }
 
     cachedResource->setLoading(true);
-    SharedBuffer* data = archiveResource->data();
     cachedResource->responseReceived(archiveResource->response());
-    cachedResource->appendData(data->data(), data->size());
+    SharedBuffer* data = archiveResource->data();
+    if (data)
+        cachedResource->appendData(data->data(), data->size());
     cachedResource->finish();
     return true;
 }

LayoutTests/mhtml/new-image-not-in-archive-expected.txt
@@ -1,0 +1,1 @@
+This tests that we don't crash if the page contains script that tries to add a resource that isn't in the archive.

LayoutTests/mhtml/new-image-not-in-archive.mht
@@ -1,0 +1,19 @@
+From: <Saved by WebKit>
+Subject: Google
+Date: Fri, 20 Jun 2013 14:36:21 -0700
+MIME-Version: 1.0
+Content-Type: multipart/related;
+	type="text/html";
+	boundary="----=_NextPart_000_CA35_2BC72789.A73F63D7"
+
+------=_NextPart_000_CA35_2BC72789.A73F63D7
+Content-Type: text/html
+Content-Transfer-Encoding: quoted-printable
+Content-Location: http://localhost/new-image-not-in-archive.html
+
+<body onload="new Image().src='resources/not-in-mhtml-archive.png'">
+<script> if (window.testRunner) { testRunner.dumpAsText() }</script>
+This tests that we don't crash if the page contains script that tries
+to add a resource that isn't in the archive.</body>
+------=_NextPart_000_CA35_2BC72789.A73F63D7--
+