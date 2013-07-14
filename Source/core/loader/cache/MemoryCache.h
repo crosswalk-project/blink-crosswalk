8a6e90f4aae91ef4f080102e72805a6a348c9aae
kerz@chromium.org <kerz@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-14 23:26:41 +0000

    Merge 153956 "Attempt to fix crash in MemoryCache::pruneDeadReso..."
    > Attempt to fix crash in MemoryCache::pruneDeadResourcesToSize.
    > 
    > We're not entirely sure what's causing this crash, but based on the stack traces
    > we have available, the prevailing theory is that refactoring in r150701 changed
    > how we remove items from MemoryCache's LRU lists. This change means we can
    > sometimes leave deleted CachedResources in the LRU lists, leading to a crash if
    > we iterate the LRU list.
    > 
    > BUG=257190
    > 
    > Review URL: https://chromiumcodereview.appspot.com/18612011
    
    TBR=japhet@chromium.org
    
    Review URL: https://codereview.chromium.org/18741007
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1547@154199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/loader/cache/MemoryCache.h
+++ Source/core/loader/cache/MemoryCache.h
@@ -111,6 +111,7 @@ public:
     CachedResource* resourceForURL(const KURL&);
     
     void add(CachedResource*);
+    void replace(CachedResource* newResource, CachedResource* oldResource);
     void remove(CachedResource* resource) { evict(resource); }
 
     static KURL removeFragmentIdentifierIfNeeded(const KURL& originalURL);
