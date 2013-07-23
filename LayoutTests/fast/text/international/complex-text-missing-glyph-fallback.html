benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 14a535f356aa5b5dbcc292dc67ed1a0952c6961e

Workaround for missing glyphs in complex text rendering path on Vista.
Vista's combination of Uniscribe and some fonts doesn't return glyphs
with value wgDefault for characters that cannot be displayed by that
font. Instead, it returns wgBlank with the fZeroWidth attribute set.

This patch detects them and treats as missing glyphs.
This is essentially the same workaround used in Chromium's
native text fields: http://crrev.com/{135206,145046}.

BUG=120336

Review URL: https://chromiumcodereview.appspot.com/18873002

git-svn-id: svn://svn.chromium.org/blink/trunk@153776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/LayoutTests/fast/text/international/complex-text-missing-glyph-fallback-expected.html
@@ -1,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<meta charset="utf-8">
+<p>
+ Verdana does not contain Japanese Hiragana glyphs,
+ so the text should be drawn inside the parentheses below by falling back to the next font.
+</p>
+<p>[<span style="text-rendering:optimizeLegibility; font-family: sans-serif">あいうえお</span>]</p>

third_party/WebKit/LayoutTests/fast/text/international/complex-text-missing-glyph-fallback.html
@@ -1,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<meta charset="utf-8">
+<p>
+ Verdana does not contain Japanese Hiragana glyphs,
+ so the text should be drawn inside the parentheses below by falling back to the next font.
+</p>
+<p>[<span style="text-rendering:optimizeLegibility; font-family: Verdana, sans-serif">あいうえお</span>]</p>

third_party/WebKit/Source/core/platform/graphics/chromium/UniscribeHelper.cpp
@@ -658,6 +658,13 @@ bool UniscribeHelper::shape(const UChar* input,
                                           &shaping.m_logs[0], &charProps[0],
                                           &shaping.m_glyphs[0], &glyphProps[0],
                                           &generatedGlyphs);
+            if (SUCCEEDED(hr)) {
+                // If we use ScriptShapeOpenType(), visual attributes
+                // information for each characters are stored in
+                // |glyphProps[i].sva|.
+                for (int i = 0; i < generatedGlyphs; ++i)
+                    memcpy(&shaping.m_visualAttributes[i], &glyphProps[i].sva, sizeof(SCRIPT_VISATTR));
+            }
         } else {
             hr = ScriptShape(m_cachedDC, scriptCache, input, itemLength,
                              numGlyphs, &run.a,
@@ -755,12 +762,6 @@ bool UniscribeHelper::shape(const UChar* input,
   cleanup:
     shaping.m_glyphs.resize(generatedGlyphs);
     shaping.m_visualAttributes.resize(generatedGlyphs);
-    // If we use ScriptShapeOpenType(), visual attributes information for each
-    // characters are stored in |glyphProps[i].sva|.
-    if (gScriptShapeOpenTypeFunc) {
-        for (int i = 0; i < generatedGlyphs; ++i)
-            memcpy(&shaping.m_visualAttributes[i], &glyphProps[i].sva, sizeof(SCRIPT_VISATTR));
-    }
     shaping.m_advance.resize(generatedGlyphs);
     shaping.m_offsets.resize(generatedGlyphs);
 
@@ -1027,13 +1028,19 @@ bool UniscribeHelper::containsMissingGlyphs(const Shaping& shaping,
 {
     for (int i = 0; i < shaping.charLength(); i++) {
         UChar c = m_input[run.iCharPos + i];
-        // Skip zero-width space characters because they're not considered to be missing in a font.
+        // Skip zero-width space characters because they're not considered to
+        // be missing in a font.
         if (Font::treatAsZeroWidthSpaceInComplexScript(c))
             continue;
         int glyphIndex = shaping.m_logs[i];
         WORD glyph = shaping.m_glyphs[glyphIndex];
+        // Note on the thrid condition: Windows Vista sometimes returns glyphs
+        // equal to wgBlank (instead of wgDefault), with fZeroWidth set. Treat
+        // such cases as having missing glyphs if the corresponding character
+        // is not a zero width whitespace.
         if (glyph == properties->wgDefault
-            || (glyph == properties->wgInvalid && glyph != properties->wgBlank))
+            || (glyph == properties->wgInvalid && glyph != properties->wgBlank)
+            || (glyph == properties->wgBlank && shaping.m_visualAttributes[glyphIndex].fZeroWidth))
             return true;
     }
     return false;

crosswalk-project/blink-crosswalk

commit 54b94b4afb0de21ff5969cfc627e34b0e87420ce

Merge 154727 "&zwj; and &zwnj; should not be treated as missing ..."
> &zwj; and &zwnj; should not be treated as missing glyphs.
> 
> This is for fixing the regression I introduced in r153776, which meant to be
> a workaround against Windows Vista that sometimes return zero-width glyph for
> a missing glyph. However, obviously, truely zero-width characters should not
> be regarded as missing glyphs. Most of such characters were already excepted out,
> but &zwj; and &zwnj; were not handled.
> 
> BUG=262113, 120336
> 
> Review URL: https://chromiumcodereview.appspot.com/20019002

TBR=kinaba@chromium.org

Review URL: https://codereview.chromium.org/19727003

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1547@154791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/platform/graphics/chromium/UniscribeHelper.cpp
@@ -1039,7 +1039,7 @@ bool UniscribeHelper::containsMissingGlyphs(const Shaping& shaping,
         // is not a zero width whitespace.
         if (glyph == properties->wgDefault
             || (glyph == properties->wgInvalid && glyph != properties->wgBlank)
-            || (glyph == properties->wgBlank && shaping.m_visualAttributes[glyphIndex].fZeroWidth))
+            || (glyph == properties->wgBlank && shaping.m_visualAttributes[glyphIndex].fZeroWidth && !Font::treatAsZeroWidthSpace(c)))
             return true;
     }
     return false;

LayoutTests/fast/text/international/complex-text-missing-glyph-fallback-expected.html
@@ -4,3 +4,8 @@
  so the text should be drawn inside the parentheses below by falling back to the next font.
 </p>
 <p>[<span style="text-rendering:optimizeLegibility; font-family: sans-serif">あいうえお</span>]</p>
+<p>
+ Zero-width spaces should not be treated as missing glyphs.
+</p>
+<p>[<span style="font-family: Arial, Tahoma">&#x633;</span>]</p>
+<p>[<span style="text-rendering:optimizeLegibility; font-family: Arial, Tahoma">AB</span>]</p>

LayoutTests/fast/text/international/complex-text-missing-glyph-fallback.html
@@ -4,3 +4,8 @@
  so the text should be drawn inside the parentheses below by falling back to the next font.
 </p>
 <p>[<span style="text-rendering:optimizeLegibility; font-family: Verdana, sans-serif">あいうえお</span>]</p>
+<p>
+ Zero-width spaces should not be treated as missing glyphs.
+</p>
+<p>[<span style="font-family: Arial, Tahoma">&#x633;&zwnj;</span>]</p>
+<p>[<span style="text-rendering:optimizeLegibility; font-family: Arial, Tahoma">AB&#8203;</span>]</p>