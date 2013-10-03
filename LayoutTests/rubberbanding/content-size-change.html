crosswalk-project/chromium-crosswalk

commit 239d97d177740d84de03ba1a7bde4ffdb3aae5f6

Mac: Fix scroll offset not being restored when using back-swipe gesture.
ScrollElasticityController::handleWheelEvent() is changed to only return
true for the PlatformWheelEventPhaseEnded case if a rubber band was actually
started (so that it doesn't return true when the start of rubber-banding
did not happen due to !shouldRubberBandInHorizontalDirection()).

EventHandler::handleWheelEvent() is changed to only set |wasScrolledByUser|
when returning true. Also, some clean up.

The underlying issue was caused by the fact that wasScrolledByUser() was
true in HistoryController::restoreScrollPositionAndViewState(), which caused
it to not restore the scroll state.

BUG=141130
TEST=On a Mac 10.7+, go to https://github.com/mojombo/jekyll/wiki/sites
and scroll down a bit. Click on a link. Gesture-scroll left via the
trackpad to trigger back navigation. The previous page should have the
same scroll position as before.

Review URL: https://chromiumcodereview.appspot.com/16336016

git-svn-id: svn://svn.chromium.org/blink/trunk@152374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/platform/mac/ScrollElasticityController.mm
@@ -141,8 +141,11 @@ bool ScrollElasticityController::handleWheelEvent(const PlatformWheelEvent& whee
     }
 
     if (wheelEvent.phase() == PlatformWheelEventPhaseEnded) {
-        snapRubberBand();
-        return true;
+        if (isRubberBandInProgress()) {
+            snapRubberBand();
+            return true;
+        }
+        return false;
     }
 
     bool isMomentumScrollEvent = (wheelEvent.momentumPhase() != PlatformWheelEventPhaseNone);

third_party/WebKit/Source/core/page/EventHandler.cpp
@@ -2119,10 +2119,15 @@ bool EventHandler::shouldTurnVerticalTicksIntoHorizontal(const HitTestResult& re
 
 bool EventHandler::handleWheelEvent(const PlatformWheelEvent& e)
 {
+#define RETURN_WHEEL_EVENT_HANDLED() \
+    { \
+        setFrameWasScrolledByUser(); \
+        return true; \
+    }
+
     Document* doc = m_frame->document();
 
-    RenderObject* docRenderer = doc->renderer();
-    if (!docRenderer)
+    if (!doc->renderer())
         return false;
     
     RefPtr<FrameView> protector(m_frame->view());
@@ -2130,14 +2135,12 @@ bool EventHandler::handleWheelEvent(const PlatformWheelEvent& e)
     FrameView* view = m_frame->view();
     if (!view)
         return false;
-    setFrameWasScrolledByUser();
+
     LayoutPoint vPoint = view->windowToContents(e.position());
 
     HitTestRequest request(HitTestRequest::ReadOnly | HitTestRequest::DisallowShadowContent);
     HitTestResult result(vPoint);
     doc->renderView()->hitTest(request, result);
-
-    bool useLatchedWheelEventNode = e.useLatchedEventNode();
 
     Node* node = result.innerNode();
     // Wheel events should not dispatch to text nodes.
@@ -2145,7 +2148,7 @@ bool EventHandler::handleWheelEvent(const PlatformWheelEvent& e)
         node = EventPathWalker::parent(node);
 
     bool isOverWidget;
-    if (useLatchedWheelEventNode) {
+    if (e.useLatchedEventNode()) {
         if (!m_latchedWheelEventNode) {
             m_latchedWheelEventNode = node;
             m_widgetIsLatched = result.isOverWidget();
@@ -2176,20 +2179,22 @@ bool EventHandler::handleWheelEvent(const PlatformWheelEvent& e)
         if (isOverWidget && target && target->isWidget()) {
             Widget* widget = toRenderWidget(target)->widget();
             if (widget && passWheelEventToWidget(e, widget))
-                return true;
+                RETURN_WHEEL_EVENT_HANDLED();
         }
 
         if (node && !node->dispatchWheelEvent(event))
-            return true;
+            RETURN_WHEEL_EVENT_HANDLED();
     }
 
 
     // We do another check on the frame view because the event handler can run JS which results in the frame getting destroyed.
     view = m_frame->view();
-    if (!view)
+    if (!view || !view->wheelEvent(event))
         return false;
-    
-    return view->wheelEvent(event);
+
+    RETURN_WHEEL_EVENT_HANDLED();
+
+#undef RETURN_WHEEL_EVENT_HANDLED
 }
 
 void EventHandler::defaultWheelEventHandler(Node* startNode, WheelEvent* wheelEvent)

crosswalk-project/blink-crosswalk

commit 388f0f762d2c79a3f945336eb533484137db00c1

Merge 158284 "[Mac] Fix rubberband overhang appearing after a co..."
> [Mac] Fix rubberband overhang appearing after a content size change.
> 
> This was caused by r152374, which changed the wheel phase end event to
> not always call snapRubberBand(). Which resulted in some state persisting
> even after the scroll, such as m_inScrollGesture being true, causing issues
> later.
> 
> For example, it would cause ScrollView::updateScrollbars() to later believe
> rubber banding was in progress and not reset the scroll position on a
> content size change (leading to this bug).
> 
> This CL changes the code to always call snapRubberBand() on a phase end
> event, like before r152374.
> 
> Additionally, it makes snapRubberBand() to not start a timer to revert the
> state if there's no overhang or momentum scroll, which removes a race (i.e.
> a resize before the timer fires) and makes the test more straight forward.
> 
> BUG=290590
> TEST=See bug.
> 
> Review URL: https://chromiumcodereview.appspot.com/24284003

TBR=asvitkine@chromium.org

Review URL: https://codereview.chromium.org/25917002

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1650@158832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/platform/mac/ScrollElasticityController.mm
@@ -132,11 +132,12 @@ bool ScrollElasticityController::handleWheelEvent(const PlatformWheelEvent& whee
     }
 
     if (wheelEvent.phase() == PlatformWheelEventPhaseEnded) {
-        if (isRubberBandInProgress()) {
-            snapRubberBand();
-            return true;
-        }
-        return false;
+        bool wasRubberBandInProgress = isRubberBandInProgress();
+        // Call snapRubberBand() even if isRubberBandInProgress() is false. For example,
+        // m_inScrollGesture may be true (and needs to be reset on a phase end) even if
+        // isRubberBandInProgress() is not (e.g. the overhang area is empty).
+        snapRubberBand();
+        return wasRubberBandInProgress;
     }
 
     bool isMomentumScrollEvent = (wheelEvent.momentumPhase() != PlatformWheelEventPhaseNone);
@@ -319,7 +320,6 @@ void ScrollElasticityController::snapRubberBandTimerFired()
 
                 m_stretchScrollForce = FloatSize();
                 m_startTime = 0;
-                m_startStretch = FloatSize();
                 m_origOrigin = FloatPoint();
                 m_origVelocity = FloatSize();
                 return;
@@ -392,11 +392,18 @@ void ScrollElasticityController::snapRubberBand()
     if (m_snapRubberbandTimerIsActive)
         return;
 
-    m_startTime = [NSDate timeIntervalSinceReferenceDate];
     m_startStretch = FloatSize();
     m_origOrigin = FloatPoint();
     m_origVelocity = FloatSize();
 
+    // If there's no momentum scroll or stretch amount, no need to start the timer.
+    if (!m_momentumScrollInProgress && m_client->stretchAmount() == FloatSize()) {
+        m_startTime = 0;
+        m_stretchScrollForce = FloatSize();
+        return;
+    }
+
+    m_startTime = [NSDate timeIntervalSinceReferenceDate];
     m_client->startSnapRubberbandTimer();
     m_snapRubberbandTimerIsActive = true;
 }

LayoutTests/platform/mac/rubberbanding/content-size-change-expected.txt
@@ -1,0 +1,9 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x538
+  RenderBlock {HTML} at (0,0) size 800x538
+    RenderBody {BODY} at (8,8) size 784x522
+      RenderBlock {DIV} at (0,0) size 788x504 [bgcolor=#008000] [border: (2px solid #000000)]
+      RenderBlock {DIV} at (0,504) size 784x18
+        RenderText {#text} at (0,0) size 148x18
+          text run at (0,0) width 148: "This test requires DRT."

LayoutTests/rubberbanding/content-size-change.html
@@ -1,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head><title>Tests changing content size after a gesture event doesn't result in overhang</title></head>
+<body>
+<div id="box" style="height:800px; background: green; border:2px solid black; width:100%"></div>
+<div id="info">This test requires DRT.</div>
+<script>
+    if (window.internals) {
+        document.getElementById('info').style.visibility = 'hidden';
+        internals.settings.setMockScrollbarsEnabled(false);
+        eventSender.mouseMoveTo(10, 10);
+        eventSender.mouseDragBegin();
+        eventSender.continuousMouseScrollBy(0, -300);
+        eventSender.mouseDragEnd();
+        document.getElementById('box').style.height = '500px';
+    }
+</script>
+</body>
+</html>

Source/testing/runner/EventSender.cpp
@@ -278,6 +278,7 @@ EventSender::EventSender(TestInterfaces* interfaces)
     bindMethod("mouseScrollBy", &EventSender::mouseScrollBy);
     bindMethod("mouseUp", &EventSender::mouseUp);
     bindMethod("mouseDragBegin", &EventSender::mouseDragBegin);
+    bindMethod("mouseDragEnd", &EventSender::mouseDragEnd);
     bindMethod("releaseTouchPoint", &EventSender::releaseTouchPoint);
     bindMethod("scheduleAsynchronousClick", &EventSender::scheduleAsynchronousClick);
     bindMethod("scheduleAsynchronousKeyDown", &EventSender::scheduleAsynchronousKeyDown);
@@ -1091,6 +1092,15 @@ void EventSender::mouseDragBegin(const CppArgumentList& arguments, CppVariant* r
     WebMouseWheelEvent event;
     initMouseEvent(WebInputEvent::MouseWheel, WebMouseEvent::ButtonNone, lastMousePos, &event, getCurrentEventTimeSec(m_delegate));
     event.phase = WebMouseWheelEvent::PhaseBegan;
+    event.hasPreciseScrollingDeltas = true;
+    webview()->handleInputEvent(event);
+}
+
+void EventSender::mouseDragEnd(const CppArgumentList& arguments, CppVariant* result)
+{
+    WebMouseWheelEvent event;
+    initMouseEvent(WebInputEvent::MouseWheel, WebMouseEvent::ButtonNone, lastMousePos, &event, getCurrentEventTimeSec(m_delegate));
+    event.phase = WebMouseWheelEvent::PhaseEnded;
     event.hasPreciseScrollingDeltas = true;
     webview()->handleInputEvent(event);
 }

Source/testing/runner/EventSender.h
@@ -90,6 +90,7 @@ public:
     void setPageScaleFactor(const CppArgumentList&, CppVariant*);
 
     void mouseDragBegin(const CppArgumentList&, CppVariant*);
+    void mouseDragEnd(const CppArgumentList&, CppVariant*);
     void mouseScrollBy(const CppArgumentList&, CppVariant*);
     void continuousMouseScrollBy(const CppArgumentList&, CppVariant*);
     void scheduleAsynchronousClick(const CppArgumentList&, CppVariant*);