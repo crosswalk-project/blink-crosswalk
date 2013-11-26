2f44fe50235a33d359dcbc8f916bb48af3657034
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-26 23:50:58 +0000

    Merge 162494 "Find the target frame for a form submission before..."
    > Find the target frame for a form submission before scheduling.
    > 
    > Form submissions appear to need their special-case of finding the target at
    > schedule rather than at fire, given the steady stream of comapt bugs since this
    > behavior went to stable.
    > 
    > BUG=319281,321550,320160
    > 
    > Review URL: https://codereview.chromium.org/81593002
    
    TBR=japhet@chromium.org
    
    Review URL: https://codereview.chromium.org/89703002
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1700@162712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/loader/NavigationScheduler.cpp
+++ Source/core/loader/NavigationScheduler.cpp
@@ -215,7 +215,7 @@ private:
 class ScheduledFormSubmission : public ScheduledNavigation {
 public:
     ScheduledFormSubmission(PassRefPtr<FormSubmission> submission, bool lockBackForwardList)
-        : ScheduledNavigation(0, lockBackForwardList, submission->target().isNull())
+        : ScheduledNavigation(0, lockBackForwardList, true)
         , m_submission(submission)
         , m_haveToldClient(false)
     {
