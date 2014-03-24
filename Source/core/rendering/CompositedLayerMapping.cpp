e5e8c5c26557427f2f2bc08de9b2c192c652593e
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-24 22:14:56 +0000

    Merge 169624 "Fix pixel snapping of clipLayers and their descend..."
    > Fix pixel snapping of clipLayers and their descendants
    >
    > Hopefully the last in a line of compositing sub-pixel
    > patches. This correctly pixel snaps the clipping layer
    > used by composited layers with overflow: clip and
    > accumulates it into their children.
    >
    > BUG=347521
    >
    > Review URL: https://codereview.chromium.org/205213006
    
    TBR=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/210383004
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1847@169882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/rendering/CompositedLayerMapping.cpp
+++ Source/core/rendering/CompositedLayerMapping.cpp
@@ -672,7 +672,7 @@ void CompositedLayerMapping::updateGraphicsLayerGeometry()
         // If the compositing ancestor has a layer to clip children, we parent in that, and therefore
         // position relative to it.
         IntRect clippingBox = clipBox(toRenderBox(compAncestor->renderer()));
-        graphicsLayerParentLocation = clippingBox.location();
+        graphicsLayerParentLocation = clippingBox.location() + roundedIntSize(compAncestor->compositedLayerMapping()->subpixelAccumulation());
     } else if (compAncestor) {
         graphicsLayerParentLocation = ancestorCompositingBounds.location();
     } else {
@@ -716,7 +716,7 @@ void CompositedLayerMapping::updateGraphicsLayerGeometry()
     IntRect clippingBox;
     if (GraphicsLayer* clipLayer = clippingLayer()) {
         clippingBox = clipBox(toRenderBox(renderer()));
-        clipLayer->setPosition(FloatPoint(clippingBox.location() - localCompositingBounds.location()));
+        clipLayer->setPosition(FloatPoint(clippingBox.location() - localCompositingBounds.location() + roundedIntSize(m_subpixelAccumulation)));
         clipLayer->setSize(clippingBox.size());
         clipLayer->setOffsetFromRenderer(toIntSize(clippingBox.location()));
         if (m_childClippingMaskLayer && !m_scrollingLayer) {
