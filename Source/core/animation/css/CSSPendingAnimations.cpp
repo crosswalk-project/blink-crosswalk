8144ec3535f7b6968d029597e76037889d6ceb80
karen@chromium.org <karen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-21 18:14:32 +0000

    Revert 172050 "Merge 171021 "CSS Animations: Animations should s..."
    > Merge 171021 "CSS Animations: Animations should start immediatel..."
    > 
    > > CSS Animations: Animations should start immediately when the page is not visible
    > > 
    > > In r67865[1] the starting of CSS animations and transitions was deferred
    > > until the next compositing update. This introduced a regression where
    > > animations triggered on a page which is not visible would not start
    > > until the page was made visible again.
    > > 
    > > This patch allows animations to start immediately when a page is not visible.
    > > 
    > > [1] https://src.chromium.org/viewvc/blink?view=rev&revision=167865
    > > 
    > > BUG=350892
    > > 
    > > Review URL: https://codereview.chromium.org/225093015
    > 
    > TBR=dstockwell@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/244373003
    
    TBR=karen@chromium.org
    
    Review URL: https://codereview.chromium.org/245483003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1916@172055 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/animation/css/CSSPendingAnimations.cpp
+++ Source/core/animation/css/CSSPendingAnimations.cpp
@@ -34,7 +34,6 @@
 #include "core/animation/Animation.h"
 #include "core/animation/DocumentTimeline.h"
 #include "core/frame/FrameView.h"
-#include "core/page/Page.h"
 #include "core/rendering/RenderLayer.h"
 
 namespace WebCore {
@@ -42,14 +41,6 @@ namespace WebCore {
 void CSSPendingAnimations::add(AnimationPlayer* player)
 {
     ASSERT(player->source()->isAnimation());
-
-    Page* page = player->timeline()->document()->page();
-    bool visible = page && page->visibilityState() == PageVisibilityStateVisible;
-    if (!player->hasStartTime() && !visible) {
-        player->setStartTime(player->timeline()->currentTime());
-        return;
-    }
-
     m_pending.append(player);
 }
 
