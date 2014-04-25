2bfe6de9538cc6f22801d790351528ff6d34a341
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-25 23:14:18 +0000

    Merge 170731 "Fix setPreferredLogicalWidthsDirty when a RenderOb..."
    > Fix setPreferredLogicalWidthsDirty when a RenderObject is already dirty.
    > 
    > Previously, RenderObject::setPreferredLogicalWidthsDirty with
    > MarkingBehavior=MarkContainingBlockChain would not dirty its ancestors
    > if the RenderObject was already marked as dirty. This patch fixes this
    > bug by ignoring whether the RenderObject was already marked as dirty.
    > 
    > Review URL: https://codereview.chromium.org/221823010
    
    TBR=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/256843005
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1916@172681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/rendering/RenderObject.cpp
+++ Source/core/rendering/RenderObject.cpp
@@ -706,9 +706,8 @@ void RenderObject::checkBlockPositionedObjectsNeedLayout()
 
 void RenderObject::setPreferredLogicalWidthsDirty(MarkingBehavior markParents)
 {
-    bool alreadyDirty = preferredLogicalWidthsDirty();
     m_bitfields.setPreferredLogicalWidthsDirty(true);
-    if (!alreadyDirty && markParents == MarkContainingBlockChain && (isText() || !style()->hasOutOfFlowPosition()))
+    if (markParents == MarkContainingBlockChain && (isText() || !style()->hasOutOfFlowPosition()))
         invalidateContainerPreferredLogicalWidths();
 }
 
