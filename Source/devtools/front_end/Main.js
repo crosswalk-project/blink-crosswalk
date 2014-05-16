primiano/blink-gitcs

commit 6cc636b800ba20769bfc7d3460791b36c161f188

DevTools: Split modules.js into per-module descriptors
R=pfeldman, vsevik

Review URL: https://codereview.chromium.org/258373005

git-svn-id: svn://svn.chromium.org/blink/trunk@173520 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/Main.js
@@ -52,9 +52,9 @@ WebInspector.Main.prototype = {
     {
         var configuration;
         if (!Capabilities.isMainFrontend) {
-            configuration = ["main", "sources", "timeline", "profiles", "console", "source_frame", "search"];
+            configuration = ["main", "sources", "timeline", "profiler", "console", "source_frame", "search"];
         } else {
-            configuration = ["main", "elements", "network", "sources", "timeline", "profiles", "resources", "audits", "console", "source_frame", "extensions", "settings", "search"];
+            configuration = ["main", "elements", "network", "sources", "timeline", "profiler", "resources", "audits", "console", "source_frame", "extensions", "settings", "search"];
             if (WebInspector.experimentsSettings.layersPanel.isEnabled())
                 configuration.push("layers");
             if (WebInspector.experimentsSettings.devicesPanel.isEnabled())

Source/devtools/devtools.gyp
@@ -51,6 +51,7 @@
                 'build_profiler_module',
                 'build_resources_module',
                 'build_search_module',
+                'build_settings_module',
                 'build_source_frame_module',
                 'build_sources_module',
                 'build_timeline_module',
@@ -249,7 +250,8 @@
                     'dependencies': [
                         'devtools_html',
                         'supported_css_properties',
-                        'frontend_protocol_sources'
+                        'frontend_protocol_sources',
+                        'concatenated_module_descriptors',
                     ],
                     'actions': [{
                         'action_name': 'build_core_module',
@@ -260,11 +262,12 @@
                             '<@(_input_page)',
                             '<@(devtools_core_js_files)',
                             '<(SHARED_INTERMEDIATE_DIR)/blink/InspectorBackendCommands.js',
-                            '<(SHARED_INTERMEDIATE_DIR)/blink/SupportedCSSProperties.js'
+                            '<(SHARED_INTERMEDIATE_DIR)/blink/SupportedCSSProperties.js',
+                            '<(SHARED_INTERMEDIATE_DIR)/blink/common/modules.js',
                         ],
                         'search_path': [
-                            'front_end',
                             '<(SHARED_INTERMEDIATE_DIR)/blink',
+                            'front_end',
                         ],
                         'outputs': ['<(PRODUCT_DIR)/resources/inspector/Main.js'],
                         'action': ['python', '<@(_script_name)', '<@(_input_page)', '<@(_search_path)', '<@(_outputs)'],
@@ -290,6 +293,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/components',
                             'files': [
                                 '<@(devtools_components_js_files)',
+                                'front_end/components/module.json',
                             ],
                         },
                         {
@@ -302,6 +306,12 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/ui',
                             'files': [
                                 '<@(devtools_ui_js_files)',
+                            ],
+                        },
+                        {
+                            'destination': '<(PRODUCT_DIR)/resources/inspector/main',
+                            'files': [
+                                'front_end/main/module.json',
                             ],
                         },
                     ]
@@ -331,6 +341,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/console',
                             'files': [
                                 '<@(devtools_console_js_files)',
+                                'front_end/console/module.json',
                             ],
                         }
                     ]
@@ -360,6 +371,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/search',
                             'files': [
                                 '<@(devtools_search_js_files)',
+                                'front_end/search/module.json',
                             ],
                         }
                     ]
@@ -389,6 +401,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/devices',
                             'files': [
                                 '<@(devtools_devices_js_files)',
+                                'front_end/devices/module.json',
                             ],
                         }
                     ]
@@ -418,6 +431,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/elements',
                             'files': [
                                 '<@(devtools_elements_js_files)',
+                                'front_end/elements/module.json',
                             ],
                         }
                     ]
@@ -447,6 +461,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/resources',
                             'files': [
                                 '<@(devtools_resources_js_files)',
+                                'front_end/resources/module.json',
                             ],
                         }
                     ]
@@ -476,6 +491,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/network',
                             'files': [
                                 '<@(devtools_network_js_files)',
+                                'front_end/network/module.json',
                             ],
                         }
                     ]
@@ -505,6 +521,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/extensions',
                             'files': [
                                 '<@(devtools_extensions_js_files)',
+                                'front_end/extensions/module.json',
                             ],
                         }
                     ]
@@ -535,6 +552,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/source_frame',
                             'files': [
                                 '<@(devtools_source_frame_js_files)',
+                                'front_end/source_frame/module.json',
                             ],
                         },
                         {
@@ -570,6 +588,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/sources',
                             'files': [
                                 '<@(devtools_sources_js_files)',
+                                'front_end/sources/module.json',
                             ],
                         }
                     ]
@@ -599,6 +618,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/timeline',
                             'files': [
                                 '<@(devtools_timeline_js_files)',
+                                'front_end/timeline/module.json',
                             ],
                         }
                     ]
@@ -628,6 +648,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/profiler',
                             'files': [
                                 '<@(devtools_profiler_js_files)',
+                                'front_end/profiler/module.json',
                             ],
                         }
                     ]
@@ -657,6 +678,25 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/audits',
                             'files': [
                                 '<@(devtools_audits_js_files)',
+                                'front_end/audits/module.json',
+                            ],
+                        }
+                    ]
+                }]
+            ]
+        },
+        {
+            'target_name': 'build_settings_module',
+            'type': 'none',
+            'conditions': [
+                ['debug_devtools==0', { # Release
+                },
+                { # Debug
+                    'copies': [
+                        {
+                            'destination': '<(PRODUCT_DIR)/resources/inspector/settings',
+                            'files': [
+                                'front_end/settings/module.json',
                             ],
                         }
                     ]
@@ -781,6 +821,7 @@
                             'destination': '<(PRODUCT_DIR)/resources/inspector/layers',
                             'files': [
                                 '<@(devtools_layers_js_files)',
+                                'front_end/layers/module.json',
                             ],
                         }
                     ]
@@ -815,6 +856,40 @@
                         'files': [
                             '<@(devtools_standalone_files)',
                         ],
+                    }],
+                },
+                {
+                    'target_name': 'concatenated_module_descriptors',
+                    'type': 'none',
+                    'actions': [{
+                        'action_name': 'concatenated_module_descriptors',
+                        'script_name': 'scripts/concatenate_module_descriptors.py',
+                        'input_file': ['front_end/common/modules.js'],
+                        'module_json_files': [
+                            'front_end/audits/module.json',
+                            'front_end/components/module.json',
+                            'front_end/console/module.json',
+                            'front_end/devices/module.json',
+                            'front_end/elements/module.json',
+                            'front_end/extensions/module.json',
+                            'front_end/layers/module.json',
+                            'front_end/main/module.json',
+                            'front_end/network/module.json',
+                            'front_end/profiler/module.json',
+                            'front_end/resources/module.json',
+                            'front_end/search/module.json',
+                            'front_end/settings/module.json',
+                            'front_end/source_frame/module.json',
+                            'front_end/sources/module.json',
+                            'front_end/timeline/module.json',
+                        ],
+                        'inputs': [
+                            '<@(_script_name)',
+                            '<@(_input_file)',
+                            '<@(_module_json_files)',
+                        ],
+                        'outputs': ['<(SHARED_INTERMEDIATE_DIR)/blink/common/modules.js'],
+                        'action': ['python', '<@(_script_name)', '<@(_input_file)', '<@(_outputs)', '<@(_module_json_files)'],
                     }],
                 },
             ],

Source/devtools/front_end/common/ModuleManager.js
@@ -138,8 +138,14 @@ WebInspector.ModuleManager.prototype = {
      */
     registerModule: function(moduleName)
     {
-        if (!this._descriptorsMap[moduleName])
-            throw new Error("Module is not defined: " + moduleName + " " + new Error().stack);
+        if (!this._descriptorsMap[moduleName]) {
+            var content = loadResource(moduleName + "/module.json");
+            if (!content)
+                throw new Error("Module is not defined: " + moduleName + " " + new Error().stack);
+            var module = /** @type {!WebInspector.ModuleManager.ModuleDescriptor} */ (self.eval("(" + content + ")"));
+            module["name"] = moduleName;
+            this._descriptorsMap[moduleName] = module;
+        }
         var module = new WebInspector.ModuleManager.Module(this, this._descriptorsMap[moduleName]);
         this._modules.push(module);
         this._modulesMap[moduleName] = module;
@@ -353,7 +359,7 @@ WebInspector.ModuleManager.Module.prototype = {
             this._manager.loadModule(dependencies[i]);
         var scripts = this._descriptor.scripts;
         for (var i = 0; scripts && i < scripts.length; ++i)
-            loadScript(scripts[i]);
+            loadScript(this._name + "/" + scripts[i]);
         this._isLoading = false;
         this._loaded = true;
     }

Source/devtools/front_end/common/utilities.js
@@ -1574,12 +1574,30 @@ function loadXHR(url, async, callback)
 var _importedScripts = {};
 
 /**
+ * @param {string} url
+ * @return {string}
+ */
+function loadResource(url)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    var stack = new Error().stack;
+    try {
+        xhr.send(null);
+    } catch (e) {
+        console.error(url + " -> " + stack);
+        throw e;
+    }
+    return xhr.responseText;
+}
+
+/**
  * This function behavior depends on the "debug_devtools" flag value.
  * - In debug mode it loads scripts synchronously via xhr request.
  * - In release mode every occurrence of "importScript" in the js files
- *   that have been white listed in the build system gets replaced with
+ *   that have been whitelisted in the build system gets replaced with
  *   the script source code on the compilation phase.
- *   The build system will throw an exception if it found importScript call
+ *   The build system will throw an exception if it finds an importScript() call
  *   in other files.
  *
  * To load scripts lazily in release mode call "loadScript" function.
@@ -1590,23 +1608,14 @@ function importScript(scriptName)
     var sourceURL = self._importScriptPathPrefix + scriptName;
     if (_importedScripts[sourceURL])
         return;
-    var xhr = new XMLHttpRequest();
     _importedScripts[sourceURL] = true;
-    xhr.open("GET", sourceURL, false);
-    var stack = new Error().stack;
-    try {
-        xhr.send(null);
-    } catch (e) {
-        console.error(scriptName + " -> " + stack);
-        throw e;
-    }
-    if (!xhr.responseText)
+    var scriptSource = loadResource(sourceURL);
+    if (!scriptSource)
         throw "empty response arrived for script '" + sourceURL + "'";
-
     var oldPrefix = self._importScriptPathPrefix;
     self._importScriptPathPrefix += scriptName.substring(0, scriptName.lastIndexOf("/") + 1);
     try {
-        self.eval(xhr.responseText + "\n//# sourceURL=" + sourceURL);
+        self.eval(scriptSource + "\n//# sourceURL=" + sourceURL);
     } finally {
         self._importScriptPathPrefix = oldPrefix;
     }

Source/devtools/front_end/components/HandlerRegistry.js
@@ -38,8 +38,6 @@ WebInspector.HandlerRegistry = function(setting)
     this._handlers = {};
     this._setting = setting;
     this._activeHandler = this._setting.get();
-
-    WebInspector.moduleManager.registerModule("handler-registry");
 }
 
 WebInspector.HandlerRegistry.prototype = {

Source/devtools/front_end/sdk/CPUProfilerModel.js
@@ -66,7 +66,7 @@ WebInspector.CPUProfilerModel.prototype = {
     consoleProfileFinished: function(id, scriptLocation, cpuProfile, title)
     {
         // Make sure ProfilesPanel is initialized and CPUProfileType is created.
-        WebInspector.moduleManager.loadModule("profiles");
+        WebInspector.moduleManager.loadModule("profiler");
         this._delegate.consoleProfileFinished(id, WebInspector.DebuggerModel.Location.fromPayload(this.target(), scriptLocation), cpuProfile, title);
     },
 
@@ -78,7 +78,7 @@ WebInspector.CPUProfilerModel.prototype = {
     consoleProfileStarted: function(id, scriptLocation, title)
     {
         // Make sure ProfilesPanel is initialized and CPUProfileType is created.
-        WebInspector.moduleManager.loadModule("profiles");
+        WebInspector.moduleManager.loadModule("profiler");
         this._delegate.consoleProfileStarted(id, WebInspector.DebuggerModel.Location.fromPayload(this.target(), scriptLocation), title);
     },
 

Source/devtools/front_end/ui/ShortcutRegistry.js
@@ -146,7 +146,7 @@ WebInspector.ShortcutRegistry.prototype = {
             for (var i = 0; bindings && i < bindings.length; ++i) {
                 if (!platformMatches(bindings[i].platform))
                     continue;
-                var shortcuts = bindings[i].shortcut.split(/\s+/);
+                var shortcuts = bindings[i]["shortcut"].split(/\s+/);
                 shortcuts.forEach(registerShortcut.bind(this, extension));
             }
         }

crosswalk-project/blink-crosswalk

commit 4ca5a24d6e92edfba01d790966565c9c23456996

Merge 173854 "DevTools: Add the new "components" module to the M..."
> DevTools: Add the new "components" module to the ModuleManager configuration
> 
> This is a regression from r173520
> 
> TBR=vsevik
> BUG=371393
> NOTRY=true
> 
> Review URL: https://codereview.chromium.org/279173002

TBR=apavlov@chromium.org

Review URL: https://codereview.chromium.org/287163003

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1985@174131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/Main.js
@@ -52,9 +52,9 @@ WebInspector.Main.prototype = {
     {
         var configuration;
         if (!Capabilities.isMainFrontend) {
-            configuration = ["main", "sources", "timeline", "profiler", "console", "source_frame", "search"];
+            configuration = ["components", "main", "sources", "timeline", "profiler", "console", "source_frame", "search"];
         } else {
-            configuration = ["main", "elements", "network", "sources", "timeline", "profiler", "resources", "audits", "console", "source_frame", "extensions", "settings", "search"];
+            configuration = ["components", "main", "elements", "network", "sources", "timeline", "profiler", "resources", "audits", "console", "source_frame", "extensions", "settings", "search"];
             if (WebInspector.experimentsSettings.layersPanel.isEnabled())
                 configuration.push("layers");
             if (WebInspector.experimentsSettings.devicesPanel.isEnabled())