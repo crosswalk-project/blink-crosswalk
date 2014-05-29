887f1b2a183334092ae26006b93b6541ebc87878
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-29 23:29:06 +0000

    Merge 175004 "Revert of Set proper referrer for css resource fet..."
    > Revert of Set proper referrer for css resource fetching (https://codereview.chromium.org/239353007/)
    > 
    > Reason for revert:
    > Caused regression
    > 
    > Original issue's description:
    > > Set proper referrer for css resource fetching
    > > 
    > > Fetching css resources are initiated as follows:
    > >   - Create FetchRequest
    > >   - Call ResourceFetcher::fetchXXX()
    > > 
    > > git-grep shows me that CSSImageValue, CSSImageSetValue and
    > > CSSFontFaceSrcValue are all of the resource fetch initiators
    > > which live in css/. I think that setting referrer in these
    > > classes should cover all css resource fetching.
    > > 
    > > Since resource fetching is delayed until the resource is actually
    > > needed, we need to hold the referrer somewhere.
    > > This CL modifies the css parser to set referrer to corresponding
    > > CSSValue (CSSImageValue and CSSFontFaceSrcValue) when it sees url(...).
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=172433
    > 
    > Review URL: https://codereview.chromium.org/304603006
    
    TBR=bashi@chromium.org
    
    Review URL: https://codereview.chromium.org/304993003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1985@175074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/css/parser/CSSPropertyParser.cpp
+++ Source/core/css/parser/CSSPropertyParser.cpp
@@ -352,13 +352,6 @@ inline PassRefPtrWillBeRawPtr<CSSPrimitiveValue> CSSPropertyParser::createPrimit
     return cssValuePool().createValue(value->string, CSSPrimitiveValue::CSS_STRING);
 }
 
-inline PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::createCSSImageValueWithReferrer(const String& rawValue, const KURL& url)
-{
-    RefPtrWillBeRawPtr<CSSValue> imageValue = CSSImageValue::create(rawValue, url);
-    toCSSImageValue(imageValue.get())->setReferrer(m_context.baseURL().strippedForUseAsReferrer());
-    return imageValue;
-}
-
 static inline bool isComma(CSSParserValue* value)
 {
     return value && value->unit == CSSParserValue::Operator && value->iValue == ',';
@@ -632,7 +625,7 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
             if (value->unit == CSSPrimitiveValue::CSS_URI) {
                 String uri = value->string;
                 if (!uri.isNull())
-                    image = createCSSImageValueWithReferrer(uri, completeURL(uri));
+                    image = CSSImageValue::create(uri, completeURL(uri));
             } else if (value->unit == CSSParserValue::Function && equalIgnoringCase(value->function->name, "-webkit-image-set(")) {
                 image = parseImageSet(m_valueList.get());
                 if (!image)
@@ -751,7 +744,7 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
             parsedValue = cssValuePool().createIdentifierValue(CSSValueNone);
             m_valueList->next();
         } else if (value->unit == CSSPrimitiveValue::CSS_URI) {
-            parsedValue = createCSSImageValueWithReferrer(value->string, completeURL(value->string));
+            parsedValue = CSSImageValue::create(value->string, completeURL(value->string));
             m_valueList->next();
         } else if (isGeneratedImageValue(value)) {
             if (parseGeneratedImage(m_valueList.get(), parsedValue))
@@ -2306,7 +2299,7 @@ bool CSSPropertyParser::parseContent(CSSPropertyID propId, bool important)
         RefPtrWillBeRawPtr<CSSValue> parsedValue = nullptr;
         if (val->unit == CSSPrimitiveValue::CSS_URI) {
             // url
-            parsedValue = createCSSImageValueWithReferrer(val->string, completeURL(val->string));
+            parsedValue = CSSImageValue::create(val->string, completeURL(val->string));
         } else if (val->unit == CSSParserValue::Function) {
             // attr(X) | counter(X [,Y]) | counters(X, Y, [,Z]) | -webkit-gradient(...)
             CSSParserValueList* args = val->function->args.get();
@@ -2410,7 +2403,7 @@ bool CSSPropertyParser::parseFillImage(CSSParserValueList* valueList, RefPtrWill
         return true;
     }
     if (valueList->current()->unit == CSSPrimitiveValue::CSS_URI) {
-        value = createCSSImageValueWithReferrer(valueList->current()->string, completeURL(valueList->current()->string));
+        value = CSSImageValue::create(valueList->current()->string, completeURL(valueList->current()->string));
         return true;
     }
 
@@ -4800,7 +4793,6 @@ bool CSSPropertyParser::parseFontWeight(bool important)
 bool CSSPropertyParser::parseFontFaceSrcURI(CSSValueList* valueList)
 {
     RefPtrWillBeRawPtr<CSSFontFaceSrcValue> uriValue(CSSFontFaceSrcValue::create(completeURL(m_valueList->current()->string)));
-    uriValue->setReferrer(m_context.baseURL().strippedForUseAsReferrer());
 
     CSSParserValue* value = m_valueList->next();
     if (!value) {
@@ -5896,7 +5888,7 @@ bool BorderImageParseContext::buildFromParser(CSSPropertyParser& parser, CSSProp
 
         if (!context.canAdvance() && context.allowImage()) {
             if (val->unit == CSSPrimitiveValue::CSS_URI) {
-                context.commitImage(parser.createCSSImageValueWithReferrer(val->string, parser.m_context.completeURL(val->string)));
+                context.commitImage(CSSImageValue::create(val->string, parser.m_context.completeURL(val->string)));
             } else if (isGeneratedImageValue(val)) {
                 RefPtrWillBeRawPtr<CSSValue> value = nullptr;
                 if (parser.parseGeneratedImage(parser.m_valueList.get(), value))
@@ -7184,7 +7176,7 @@ PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseImageSet(CSSParserValue
         if (arg->unit != CSSPrimitiveValue::CSS_URI)
             return nullptr;
 
-        RefPtrWillBeRawPtr<CSSValue> image = createCSSImageValueWithReferrer(arg->string, completeURL(arg->string));
+        RefPtrWillBeRawPtr<CSSImageValue> image = CSSImageValue::create(arg->string, completeURL(arg->string));
         imageSet->append(image);
 
         arg = functionArgs->next();
