990ecb74728e0809af958130c57a4d49349d1933
dmazzoni@google.com <dmazzoni@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-23 08:03:55 +0000

    Merge 176724 "Revert 170343 "Send AXLayoutComplete even when doc..."
    > Revert 170343 "Send AXLayoutComplete even when document hasn't f..."
    > 
    > Temporarily reverting so we can merge the revert to M36. Will un-revert
    > on trunk.
    > 
    > > Send AXLayoutComplete even when document hasn't finished loading yet (reland)
    > > 
    > > Because these notifications only got sent if the corresponding AXObject
    > > already existed, they were effectively never sent before the first
    > > load complete notification. This change creates the AXObject for the
    > > document element after the first layout, allowing future notifications too.
    > > 
    > > BUG=353067
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=170085
    > > 
    > > Review URL: https://codereview.chromium.org/199693003
    > 
    > TBR=dmazzoni@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/345563006
    
    TBR=dmazzoni@google.com
    
    Review URL: https://codereview.chromium.org/346773003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/1985@176731 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/frame/FrameView.cpp
+++ Source/core/frame/FrameView.cpp
@@ -980,8 +980,8 @@ void FrameView::layout(bool allowSubtree)
 
     m_layoutCount++;
 
-    if (AXObjectCache* cache = rootForThisLayout->document().axObjectCache())
-        cache->handleLayoutComplete(rootForThisLayout);
+    if (AXObjectCache* cache = rootForThisLayout->document().existingAXObjectCache())
+        cache->postNotification(rootForThisLayout, AXObjectCache::AXLayoutComplete, true);
     updateAnnotatedRegions();
 
     ASSERT(!rootForThisLayout->needsLayout());
