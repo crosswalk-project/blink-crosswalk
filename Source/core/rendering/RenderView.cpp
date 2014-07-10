crosswalk-project/blink-crosswalk

commit 6e7bb780db9b90a1f0fc380d0077a86ecb6facb0

Disable optimization of repaint on viewport resize in M37
The optimization exposed some old bugs of incremental invalidation.
Will try to fix the bugs in M38, but for now disable the optimization
in M37.

BUG=390378,258219
R=amineer@chromium.org, esprehn@chromium.org

Review URL: https://codereview.chromium.org/368653003

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2062@177852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/rendering/RenderView.cpp
@@ -201,6 +201,9 @@ bool RenderView::shouldDoFullRepaintForNextLayout() const
         return true;
 
     if (height() != viewHeight()) {
+        // FIXME: Disable optimization to fix crbug.com/390378 for the branch.
+        return true;
+
         if (RenderObject* backgroundRenderer = this->backgroundRenderer()) {
             // When background-attachment is 'fixed', we treat the viewport (instead of the 'root'
             // i.e. html or body) as the background positioning area, and we should full repaint

commit 7fa9f7011e88b7e5d9818b507c65bc57b4b237f1

Disable unreachable code caused by r177852
TBR=govind,animeer,matthewyuan

Review URL: https://codereview.chromium.org/383783002

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2062@177862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/rendering/RenderView.cpp
@@ -203,7 +203,7 @@ bool RenderView::shouldDoFullRepaintForNextLayout() const
     if (height() != viewHeight()) {
         // FIXME: Disable optimization to fix crbug.com/390378 for the branch.
         return true;
-
+#if 0
         if (RenderObject* backgroundRenderer = this->backgroundRenderer()) {
             // When background-attachment is 'fixed', we treat the viewport (instead of the 'root'
             // i.e. html or body) as the background positioning area, and we should full repaint
@@ -215,6 +215,7 @@ bool RenderView::shouldDoFullRepaintForNextLayout() const
                 return true;
             }
         }
+#endif
     }
 
     return false;