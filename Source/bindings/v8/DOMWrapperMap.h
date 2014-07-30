170d83711d1e81d5f6457e373d872a9baa53a0b1
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-30 15:31:52 +0000

    Merge 178823 "Replace further questionable HashMap::add usages i..."
    > Replace further questionable HashMap::add usages in bindings
    > 
    > BUG=390928
    > R=dcarney@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/411273002
    
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/429233002
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2062@179238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/bindings/v8/DOMWrapperMap.h
+++ Source/bindings/v8/DOMWrapperMap.h
@@ -107,7 +107,7 @@ private:
             Impl* impl, KeyType* key, v8::PersistentContainerValue value)
         {
             v8::PersistentContainerValue oldValue = Get(impl, key);
-            impl->add(key, value);
+            impl->set(key, value);
             return oldValue;
         }
         static v8::PersistentContainerValue Get(const Impl* impl, KeyType* key)
