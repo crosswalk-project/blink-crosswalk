9b93984cd1565c0102689f5420c257501be99468
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 16:38:06 +0000

    Merge 177957 "HTML Imports: A speculative fix for shutdown time ..."
    > HTML Imports: A speculative fix for shutdown time race
    > 
    > It seems possible that new imports are requested after
    > HTMLImportsController is detached from the document.
    > This change simplifies the lifetime so that the 
    > supplement is removed as Document::m_importsController
    > is cleared.
    > 
    > No test, timing dependent and hard to reproduce.
    > 
    > TEST=none
    > R=dglazkov@chromium.org, dominicc@chromium.org
    > BUG=390176
    > 
    > Review URL: https://codereview.chromium.org/369433002
    
    TBR=morrita@chromium.org
    
    Review URL: https://codereview.chromium.org/444223006
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2062@179724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/dom/Document.cpp
+++ Source/core/dom/Document.cpp
@@ -576,10 +576,8 @@ Document::~Document()
     if (m_styleSheetList)
         m_styleSheetList->detachFromDocument();
 
-    if (m_importsController) {
-        m_importsController->wasDetachedFrom(*this);
-        m_importsController = nullptr;
-    }
+    if (m_importsController)
+        HTMLImportsController::removeFrom(*this);
 
     m_timeline->detachFromDocument();
 
@@ -633,10 +631,8 @@ void Document::dispose()
 
     m_registrationContext.clear();
 
-    if (m_importsController) {
-        m_importsController->wasDetachedFrom(*this);
-        m_importsController = nullptr;
-    }
+    if (m_importsController)
+        HTMLImportsController::removeFrom(*this);
 
     // removeDetachedChildren() doesn't always unregister IDs,
     // so tear down scope information upfront to avoid having stale references in the map.
