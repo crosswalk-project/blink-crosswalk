0c304d8ee60ae74117a666897313da503b1ccf94
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-08 07:21:42 +0000

    Merge 177505 "Mixed content: treat 'javascript:' form actions as..."
    > Mixed content: treat 'javascript:' form actions as secure.
    > 
    > We currently warn for insecure form actions on secure pages. A variety
    > of libraries use 'javascript:void(0)' as a form action when handling
    > the submission via JavaScript/XHR. We shouldn't treat that as a mixed
    > content violation.
    > 
    > This CL also exempts those folks who have enabled lax mixed content
    > checking from this newly-introduced check.
    > 
    > BUG=253249
    > 
    > Review URL: https://codereview.chromium.org/369793002
    
    TBR=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/452003002
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2062@179798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/loader/MixedContentChecker.h
+++ Source/core/loader/MixedContentChecker.h
@@ -51,6 +51,11 @@ public:
     }
     bool canSubmitToInsecureForm(SecurityOrigin* securityOrigin, const KURL& url) const
     {
+        // For whatever reason, some folks handle forms via JavaScript, and submit to `javascript:void(0)`
+        // rather than calling `preventDefault()`. We special-case `javascript:` URLs here, as they don't
+        // introduce MixedContent for form submissions.
+        if (url.protocolIs("javascript"))
+            return true;
         return canDisplayInsecureContentInternal(securityOrigin, url, MixedContentChecker::Submission);
     }
 
