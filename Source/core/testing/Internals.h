b979ead61a90b841610a253cda006b6bdcc13bec
avayvod@chromium.org <avayvod@chromium.org>
2014-10-20 19:57:44 +0000

    Merge 183701 "[Cast] Disable autohide of media controls for remo..."
    > [Cast] Disable autohide of media controls for remote playback
    > 
    > BUG=420651
    > 
    > Review URL: https://codereview.chromium.org/650893002
    
    TBR=avayvod@chromium.org
    
    Review URL: https://codereview.chromium.org/652543004
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2171@184007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/testing/Internals.h
+++ Source/core/testing/Internals.h
@@ -249,7 +249,8 @@ public:
     void mediaPlayerRequestFullscreen(HTMLMediaElement*);
     double effectiveMediaVolume(HTMLMediaElement*);
 
-    void mediaPlayerRemoteRouteAvailabilityChanged(HTMLMediaElement *, bool);
+    void mediaPlayerRemoteRouteAvailabilityChanged(HTMLMediaElement*, bool);
+    void mediaPlayerPlayingRemotelyChanged(HTMLMediaElement*, bool);
 
     void registerURLSchemeAsBypassingContentSecurityPolicy(const String& scheme);
     void removeURLSchemeRegisteredAsBypassingContentSecurityPolicy(const String& scheme);
