e2ff7f4339f9f173df468bf05f3dddd565a1dad2
dmazzoni@google.com <dmazzoni@google.com>
2014-11-28 22:53:57 +0000

    Merge 185896 "Make it possible to load inline text boxes dynamic..."
    > Make it possible to load inline text boxes dynamically.
    > 
    > This change will be exercised by: https://codereview.chromium.org/743413002/
    > 
    > BUG=353438,378810
    > 
    > Review URL: https://codereview.chromium.org/700373007
    
    TBR=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/766043002
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2214@186198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/accessibility/AXRenderObject.cpp
+++ Source/core/accessibility/AXRenderObject.cpp
@@ -883,6 +883,19 @@ KURL AXRenderObject::url() const
 }
 
 //
+// Load inline text boxes.
+//
+
+void AXRenderObject::loadInlineTextBoxes()
+{
+    if (!renderer() || !renderer()->isText())
+        return;
+
+    clearChildren();
+    addInlineTextBoxChildren(true);
+}
+
+//
 // Properties of interactive elements.
 //
 
@@ -1532,7 +1545,7 @@ void AXRenderObject::addChildren()
     addTextFieldChildren();
     addCanvasChildren();
     addRemoteSVGChildren();
-    addInlineTextBoxChildren();
+    addInlineTextBoxChildren(false);
 
     for (unsigned i = 0; i < m_children.size(); ++i) {
         if (!m_children[i].get()->cachedParentObject())
@@ -1875,10 +1888,10 @@ int AXRenderObject::indexForVisiblePosition(const VisiblePosition& pos) const
     return TextIterator::rangeLength(range.get());
 }
 
-void AXRenderObject::addInlineTextBoxChildren()
+void AXRenderObject::addInlineTextBoxChildren(bool force)
 {
     Settings* settings = document()->settings();
-    if (!settings || !settings->inlineTextBoxAccessibilityEnabled())
+    if (!force && (!settings || !settings->inlineTextBoxAccessibilityEnabled()))
         return;
 
     if (!renderer() || !renderer()->isText())
