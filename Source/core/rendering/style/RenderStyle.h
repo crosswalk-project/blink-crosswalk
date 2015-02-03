dd59e6d18ffd4745c25759ef249173b872afed27
chrishtr@chromium.org <chrishtr@chromium.org>
2015-02-03 23:22:36 +0000

    Merge 189363 "Change border radius code to not violate maximum r..."
    > Change border radius code to not violate maximum radius constraint in more cases.
    > 
    > To do this, makes two changes: 
    > 1. Use floor() instead of round(), which always picks smaller radiuses rather than sometimes larger ones. The old (Int)RoundedRect code did this implicitly.
    > 2. Factor the code to apply CSS layout constraints on radii into a clean method within FloatRoundedRect.
    > 
    > BUG=452814,451996,451600,453957,452511
    > 
    > Review URL: https://codereview.chromium.org/890993002
    
    TBR=chrishtr@chromium.org
    
    Review URL: https://codereview.chromium.org/900683003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2272@189444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/rendering/style/RenderStyle.h
+++ Source/core/rendering/style/RenderStyle.h
@@ -1845,8 +1845,6 @@ inline bool RenderStyle::hasPseudoElementStyle() const
     return noninherited_flags.pseudoBits & PSEUDO_ELEMENT_MASK;
 }
 
-float calcBorderRadiiConstraintScaleFor(const FloatRect&, const FloatRoundedRect::Radii&);
-
 } // namespace blink
 
 #endif // RenderStyle_h
