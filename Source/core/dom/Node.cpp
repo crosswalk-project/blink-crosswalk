8155e81bb57bf320a0a75c9a90014a2d8b628f64
skyostil@chromium.org <skyostil@chromium.org>
2015-03-16 16:36:33 +0000

    Merge 191849 "Revert "Make whitespace re-attaching determininistic""
    > Revert "Make whitespace re-attaching determininistic"
    > 
    > This reverts commit 0aaa9d77ab0af5b4a53d0eae5bce6b7466823806.
    > 
    > Not adjusting the expectations for fast/events/mouseover-mouseout.html
    > because it seems to be stable locally now. However, with artificial
    > delays it is still flaky[1].
    > 
    > BUG=465377
    > TBR=wangxianzhu@chromium.org
    > 
    > [1] https://docs.google.com/a/chromium.org/document/d/1Yl4SnTLBWmY1O99_BTtQvuoffP8YM9HZx2YPkEsaduQ/edit#
    > 
    > Review URL: https://codereview.chromium.org/1000203006
    
    TBR=skyostil@chromium.org
    
    Review URL: https://codereview.chromium.org/1009973003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2311@191935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/core/dom/Node.cpp
+++ Source/core/dom/Node.cpp
@@ -962,7 +962,12 @@ void Node::reattachWhitespaceSiblingsIfNeeded(Text* start)
 {
     for (Node* sibling = start; sibling; sibling = sibling->nextSibling()) {
         if (sibling->isTextNode() && toText(sibling)->containsOnlyWhitespace()) {
+            bool hadLayoutObject = !!sibling->renderer();
             toText(sibling)->reattachIfNeeded();
+            // If sibling's layout object status didn't change we don't need to continue checking
+            // other siblings since their layout object status won't change either.
+            if (!!sibling->renderer() == hadLayoutObject)
+                return;
         } else if (sibling->renderer()) {
             return;
         }
