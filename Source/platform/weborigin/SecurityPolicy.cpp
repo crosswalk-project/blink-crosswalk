814e1da2a99ba2fcb8eed754da09ae8495c1e8a6
amineer@chromium.org <amineer@chromium.org>
2015-04-07 17:14:26 +0000

    Merge 192828 "Don't create referrer headers for blob and filesys..."
    > Don't create referrer headers for blob and filesystem URLs
    > 
    > In the renderer, we use the inner URL as security origin for those
    > schemes, however, in the browser, this is not done, and so the network
    > stack ends up disagreeing about what origin a URL is.
    > 
    > Since e.g. blob: technically doesn't have an inner URL anyways, we just
    > drop the referrer header for those schemes.
    > 
    > Also remove superfluous "null" checks for the referrer origin. We
    > already know it's an http or https origin, so the security origin cannot
    > be unique
    > 
    > BUG=471533
    > R=mkwst@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/1049643002
    
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/1060293003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2311@193285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/platform/weborigin/SecurityPolicy.cpp
+++ Source/platform/weborigin/SecurityPolicy.cpp
@@ -74,6 +74,9 @@ Referrer SecurityPolicy::generateReferrer(ReferrerPolicy referrerPolicy, const K
     if (!(protocolIs(referrer, "https") || protocolIs(referrer, "http")))
         return Referrer(String(), referrerPolicy);
 
+    if (SecurityOrigin::shouldUseInnerURL(url))
+        return Referrer(String(), referrerPolicy);
+
     switch (referrerPolicy) {
     case ReferrerPolicyNever:
         return Referrer(String(), referrerPolicy);
@@ -81,8 +84,6 @@ Referrer SecurityPolicy::generateReferrer(ReferrerPolicy referrerPolicy, const K
         return Referrer(referrer, referrerPolicy);
     case ReferrerPolicyOrigin: {
         String origin = SecurityOrigin::createFromString(referrer)->toString();
-        if (origin == "null")
-            return Referrer(String(), referrerPolicy);
         // A security origin is not a canonical URL as it lacks a path. Add /
         // to turn it into a canonical URL we can use as referrer.
         return Referrer(origin + "/", referrerPolicy);
@@ -92,8 +93,6 @@ Referrer SecurityPolicy::generateReferrer(ReferrerPolicy referrerPolicy, const K
         RefPtr<SecurityOrigin> urlOrigin = SecurityOrigin::create(url);
         if (!urlOrigin->isSameSchemeHostPort(referrerOrigin.get())) {
             String origin = referrerOrigin->toString();
-            if (origin == "null")
-                return Referrer(String(), referrerPolicy);
             return Referrer(origin + "/", referrerPolicy);
         }
         break;
@@ -105,8 +104,6 @@ Referrer SecurityPolicy::generateReferrer(ReferrerPolicy referrerPolicy, const K
         RefPtr<SecurityOrigin> urlOrigin = SecurityOrigin::create(url);
         if (RuntimeEnabledFeatures::reducedReferrerGranularityEnabled() && !urlOrigin->isSameSchemeHostPort(referrerOrigin.get())) {
             String origin = referrerOrigin->toString();
-            if (origin == "null")
-                return Referrer(String(), referrerPolicy);
             return Referrer(shouldHideReferrer(url, referrer) ? String() : origin + "/", referrerPolicy);
         }
         break;
