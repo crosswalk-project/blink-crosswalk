crosswalk-project/chromium-crosswalk

commit 6073897321d50364f09852a2991b74bfc886d791

Fix issue on <select> style change when popup is visible
When a user selects an item in a popup menu using the keyboard the selection is a provisional selection that isn't accepted until the popup is abandoned. The index is saved to m_acceptedIndexOnAbandon and the menulist button text is set.
When LayoutMenuList::updateFromElement() is called we need to update the menulist button text and we need to respect this provisional selection or else it looks like the selection has suddenly changed to the user.
To fix this LayoutMenuList needs to know about m_acceptedIndexOnAbandon. It turned out that the popup doesn't really care if the provisional selection or an accepted selection so I moved the provisional selection tracking to the PopupMenuClient.
LayoutMenuList needs to accept the provisional selection only when the popup was closed without accepting a selection so we add PopupMenuClient::popupDidCancel().

BUG=466004

Review URL: https://codereview.chromium.org/1013303004

git-svn-id: svn://svn.chromium.org/blink/trunk@193034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/LayoutTests/fast/forms/select/menulist-popup-type-ahead-style-change-expected.txt
@@ -1,0 +1,10 @@
+Tests <select> style change when popup is visible after selection is changed by keyboard typeahead. The selection should not change when style changes. Needs run-layout-test.
+
+layer at (0,0) size 34x20 backgroundClip at (8,50) size 34x20 clip at (8,50) size 34x20 outlineClip at (8,50) size 34x20
+  LayoutBlockFlow (positioned) {DIV} at (8,50) size 34x20
+    LayoutMenuList {SELECT} at (0,0) size 34x20 [bgcolor=#FFFF00] [border: (1px solid #A9A9A9)]
+      LayoutBlockFlow (anonymous) at (1,1) size 32x18
+        LayoutText at (4,1) size 9x16
+          text run at (4,1) width 9: "C"
+    LayoutText {#text} at (0,0) size 0x0
+

third_party/WebKit/LayoutTests/fast/forms/select/menulist-popup-type-ahead-style-change.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+Tests &lt;select&gt; style change when popup is visible after selection is changed
+by keyboard typeahead. The selection should not change when style changes.
+Needs run-layout-test.<br>
+<div style="position: absolute; top: 50px">
+  <select id="select">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+</div>
+<pre id="output"></pre>
+<script>
+if (window.testRunner && window.eventSender) {
+  testRunner.dumpAsText();
+  var select = document.getElementById('select');
+  select.focus();
+  eventSender.keyDown(' ', null);
+  select.offsetTop;
+  eventSender.keyDown('C', null);
+  select.offsetTop;
+  select.style.backgroundColor = 'yellow';
+
+  // We need to show in the result the title text of the select when popup is visible.
+  // Must dump renderer tree here otherwise run-layout-test will close the popup.
+  // The title text should be "C".
+  output.textContent = internals.elementLayoutTreeAsText(select.parentNode);
+}
+</script>

third_party/WebKit/Source/core/layout/LayoutMenuList.cpp
@@ -59,6 +59,7 @@ LayoutMenuList::LayoutMenuList(Element* element)
     , m_optionsWidth(0)
     , m_lastActiveIndex(-1)
     , m_popupIsVisible(false)
+    , m_indexToSelectOnCancel(-1)
 {
     ASSERT(isHTMLSelectElement(element));
 }
@@ -210,7 +211,9 @@ void LayoutMenuList::updateFromElement()
     if (m_popupIsVisible)
         m_popup->updateFromElement();
 
-    if (selectElement()->suggestedIndex() >= 0)
+    if (m_indexToSelectOnCancel >= 0)
+        setTextFromOption(selectElement()->listToOptionIndex(m_indexToSelectOnCancel));
+    else if (selectElement()->suggestedIndex() >= 0)
         setTextFromOption(selectElement()->suggestedIndex());
     else
         setTextFromOption(selectElement()->selectedIndex());
@@ -579,6 +582,14 @@ void LayoutMenuList::popupDidHide()
         cache->didHideMenuListPopup(this);
 }
 
+void LayoutMenuList::popupDidCancel()
+{
+    if (m_indexToSelectOnCancel >= 0) {
+        valueChanged(m_indexToSelectOnCancel);
+        m_indexToSelectOnCancel = -1;
+    }
+}
+
 bool LayoutMenuList::itemIsSeparator(unsigned listIndex) const
 {
     const WillBeHeapVector<RawPtrWillBeMember<HTMLElement>>& listItems = selectElement()->listItems();
@@ -600,9 +611,10 @@ bool LayoutMenuList::itemIsSelected(unsigned listIndex) const
     return isHTMLOptionElement(*element) && toHTMLOptionElement(*element).selected();
 }
 
-void LayoutMenuList::setTextFromItem(unsigned listIndex)
+void LayoutMenuList::provisionalSelectionChanged(unsigned listIndex)
 {
-    setTextFromOption(selectElement()->listToOptionIndex(listIndex));
+    m_indexToSelectOnCancel = listIndex;
+    setTextFromOption(selectElement()->listToOptionIndex(m_indexToSelectOnCancel));
 }
 
 } // namespace blink

third_party/WebKit/Source/core/layout/LayoutMenuList.h
@@ -90,10 +90,11 @@ private:
     virtual int listSize() const override;
     virtual int selectedIndex() const override;
     virtual void popupDidHide() override;
+    virtual void popupDidCancel() override;
     virtual bool itemIsSeparator(unsigned listIndex) const override;
     virtual bool itemIsLabel(unsigned listIndex) const override;
     virtual bool itemIsSelected(unsigned listIndex) const override;
-    virtual void setTextFromItem(unsigned listIndex) override;
+    virtual void provisionalSelectionChanged(unsigned) override;
     virtual void listBoxSelectItem(int listIndex, bool allowMultiplySelections, bool shift, bool fireOnChangeNow = true) override;
     virtual bool multiple() const override;
     virtual IntRect elementRectRelativeToViewport() const override;
@@ -133,6 +134,7 @@ private:
 
     RefPtrWillBePersistent<PopupMenu> m_popup;
     bool m_popupIsVisible;
+    int m_indexToSelectOnCancel;
 
     // TODO(tkent): Use FRIEND_TEST macro provided by gtest_prod.h
     friend class LayoutMenuListTest_ElementRectRelativeToViewport_Test;

third_party/WebKit/LayoutTests/TestExpectations
@@ -48,6 +48,8 @@ crbug.com/443780 [ Mac ] virtual/slimmingpaint/fast/events/menu-key-context-menu
 crbug.com/443780 [ Mac ] virtual/slimmingpaint/fast/events/menu-key-context-menu.html [ Failure ]
 crbug.com/435662 [ Mac ] virtual/slimmingpaint/fast/events/menu-key-context-menu-position.html [ Failure ]
 
+crbug.com/466004 fast/forms/select/menulist-popup-type-ahead-style-change.html [ NeedsRebaseline ]
+
 crbug.com/461283 [ Mac ] fast/workers/worker-multi-startup.html [ Pass Crash ]
 
 crbug.com/441840 [ Mac XP ] cssom/ahem-ex-units.html [ Failure ]

third_party/WebKit/LayoutTests/fast/forms/select/menulist-type-ahead-find-original-item-expected.txt
@@ -1,0 +1,7 @@
+Verify type ahead selection fires onchange event.
+
+Set focus to select element
+Type down arrow twice and type "a"
+You see "apple" in select element and "PASS" below select element.
+
+PASS

third_party/WebKit/LayoutTests/fast/forms/select/menulist-type-ahead-find-original-item.html
@@ -1,7 +1,9 @@
-=======
+<!DOCTYPE html>
 <html>
 <head>
 <script>
+var expectedSelectedIndex;
+
 function recordIt()
 {
     var res = document.getElementById("res");
@@ -23,17 +25,18 @@ function test()
         return;
 
     testRunner.dumpAsText();
-    testIt("c", 2);
+    testIt("downArrow", 1);
+    testIt("downArrow", 2);
+    testIt("a", 0);
 }
 </script>
 </head>
 <body onload="test()">
-<h1>WebKit Bug <a href="https://bugs.webkit.org/show_bug.cgi?id=74590">74590</a></h1>
-Verify type ahead selection fires onchange event.
+<h1>Verify type ahead selection fires onchange event.</h1>
 <ol>
 <li>Set focus to select element</li>
-<li>Type "c"</li>
-<li>You see "cherry" in select element and "PASS" below select element.</li>
+<li>Type down arrow twice and type "a"</li>
+<li>You see "apple" in select element and "PASS" below select element.</li>
 </ol>
 <select id="sel" onchange="recordIt()">
     <option>apple</option>

third_party/WebKit/LayoutTests/platform/mac/fast/forms/select/menulist-type-ahead-find-original-item-expected.txt
@@ -1,0 +1,7 @@
+Verify type ahead selection fires onchange event.
+
+Set focus to select element
+Type down arrow twice and type "a"
+You see "apple" in select element and "PASS" below select element.
+
+FAIL

crosswalk-project/blink-crosswalk

commit 41411b549706f932a8b2aec2cbde31c9b859e862

Merge 193436 "Regression: Fix LayoutMenuList from showing stale ..."
> Regression: Fix LayoutMenuList from showing stale provisional selection
> 
> When a provisional selection was set, the menulist text wasn't being updated to the actual selection.
> 
> Regression in r193034
> 
> TEST=Added to popup-menu-key-operations.html
> BUG=474471
> 
> Review URL: https://codereview.chromium.org/1070583002

TBR=keishi@chromium.org

Review URL: https://codereview.chromium.org/1058893003

git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2357@193588 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/layout/LayoutMenuList.cpp
@@ -211,6 +211,17 @@ void LayoutMenuList::updateFromElement()
     if (m_popupIsVisible)
         m_popup->updateFromElement();
 
+    updateText();
+}
+
+void LayoutMenuList::setIndexToSelectOnCancel(int listIndex)
+{
+    m_indexToSelectOnCancel = listIndex;
+    updateText();
+}
+
+void LayoutMenuList::updateText()
+{
     if (m_indexToSelectOnCancel >= 0)
         setTextFromOption(selectElement()->listToOptionIndex(m_indexToSelectOnCancel));
     else if (selectElement()->suggestedIndex() >= 0)
@@ -370,6 +381,8 @@ void LayoutMenuList::valueChanged(unsigned listIndex, bool fireOnChange)
     Document& doc = toElement(node())->document();
     if (&doc != doc.frame()->document())
         return;
+
+    setIndexToSelectOnCancel(-1);
 
     HTMLSelectElement* select = selectElement();
     select->optionSelectedByUser(select->listToOptionIndex(listIndex), fireOnChange);
@@ -586,7 +599,7 @@ void LayoutMenuList::popupDidCancel()
 {
     if (m_indexToSelectOnCancel >= 0) {
         valueChanged(m_indexToSelectOnCancel);
-        m_indexToSelectOnCancel = -1;
+        setIndexToSelectOnCancel(-1);
     }
 }
 
@@ -613,8 +626,7 @@ bool LayoutMenuList::itemIsSelected(unsigned listIndex) const
 
 void LayoutMenuList::provisionalSelectionChanged(unsigned listIndex)
 {
-    m_indexToSelectOnCancel = listIndex;
-    setTextFromOption(selectElement()->listToOptionIndex(m_indexToSelectOnCancel));
+    setIndexToSelectOnCancel(listIndex);
 }
 
 } // namespace blink

Source/core/layout/LayoutMenuList.h
@@ -119,6 +119,8 @@ private:
     void setText(const String&);
     void setTextFromOption(int optionIndex);
     void updateOptionsWidth();
+    void updateText();
+    void setIndexToSelectOnCancel(int listIndex);
 
     void didUpdateActiveOption(int optionIndex);
 

LayoutTests/fast/forms/select/menulist-popup-type-ahead-style-change-expected.txt
@@ -1,10 +1,7 @@
+PASS internals.selectMenuListText(select) is "C"
+PASS successfullyParsed is true
+
+TEST COMPLETE
 Tests <select> style change when popup is visible after selection is changed by keyboard typeahead. The selection should not change when style changes. Needs run-layout-test.
 
-layer at (0,0) size 34x20 backgroundClip at (8,50) size 34x20 clip at (8,50) size 34x20 outlineClip at (8,50) size 34x20
-  LayoutBlockFlow (positioned) {DIV} at (8,50) size 34x20
-    LayoutMenuList {SELECT} at (0,0) size 34x20 [bgcolor=#FFFF00] [border: (1px solid #A9A9A9)]
-      LayoutBlockFlow (anonymous) at (1,1) size 32x18
-        LayoutText at (4,1) size 9x16
-          text run at (4,1) width 9: "C"
-    LayoutText {#text} at (0,0) size 0x0
 

LayoutTests/fast/forms/select/menulist-popup-type-ahead-style-change.html
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
+<html>
+<head>
+<script src="../../../resources/js-test.js"></script>
+<script src="../resources/common.js"></script>
+<script src="../resources/picker-common.js"></script>
+</head>
+<body>
 Tests &lt;select&gt; style change when popup is visible after selection is changed
 by keyboard typeahead. The selection should not change when style changes.
 Needs run-layout-test.<br>
-<div style="position: absolute; top: 50px">
-  <select id="select">
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
-</div>
+<select id="select">
+  <option>A</option>
+  <option>B</option>
+  <option>C</option>
+</select>
 <pre id="output"></pre>
 <script>
-if (window.testRunner && window.eventSender) {
-  testRunner.dumpAsText();
-  var select = document.getElementById('select');
-  select.focus();
-  eventSender.keyDown(' ', null);
-  select.offsetTop;
-  eventSender.keyDown('C', null);
-  select.offsetTop;
-  select.style.backgroundColor = 'yellow';
-
-  // We need to show in the result the title text of the select when popup is visible.
-  // Must dump renderer tree here otherwise run-layout-test will close the popup.
-  // The title text should be "C".
-  output.textContent = internals.elementLayoutTreeAsText(select.parentNode);
+var select = document.getElementById('select');
+function openPickerErrorCallback() {
+    testFailed('picker didn\'t open')
+    finishJSTest();
 }
+openPicker(select, function() {
+    eventSender.keyDown('C', null);
+    select.offsetTop;
+    select.style.backgroundColor = 'yellow';
+    shouldBeEqualToString('internals.selectMenuListText(select)', 'C');
+    finishJSTest();
+}, openPickerErrorCallback);
 </script>
+</body>
+</html>
+

LayoutTests/fast/forms/select/popup-menu-key-operations.html
@@ -15,7 +15,7 @@
   </optgroup>
 </select>
 <script>
-var menuElement = document.getElementById('menu');
+var menu = document.getElementById('menu');
 var picker = null;
 function openPickerErrorCallback() {
     testFailed('picker didn\'t open')
@@ -26,31 +26,38 @@ openPicker(menu, test1, openPickerErrorCallback);
 function test1() {
     picker = window.internals.pagePopupWindow.global.picker;
     shouldBeEqualToString('picker._selectElement.value', '1');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'bar');
 
     eventSender.keyDown('downArrow');
     shouldBeEqualToString('picker._selectElement.value', '4');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     eventSender.keyDown('downArrow');
     shouldBeEqualToString('picker._selectElement.value', '4');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     eventSender.keyDown('upArrow');
     shouldBeEqualToString('picker._selectElement.value', '1');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'bar');
 
     eventSender.keyDown('upArrow');
     shouldBeEqualToString('picker._selectElement.value', '0');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
 
     eventSender.keyDown('upArrow');
     shouldBeEqualToString('picker._selectElement.value', '0');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
 
     eventSender.keyDown('\n');
     shouldBeNull('window.internals.pagePopupWindow');
-    shouldBeEqualToString('menuElement.value', 'foo');
+    shouldBeEqualToString('menu.value', 'foo');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
 
     waitUntilClosing(function() {
         openPicker(menu, test2, openPickerErrorCallback);
@@ -60,15 +67,18 @@ function test1() {
 function test2() {
     picker = window.internals.pagePopupWindow.global.picker;
     shouldBeEqualToString('picker._selectElement.value', '0');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
 
     eventSender.keyDown('downArrow');
     shouldBeEqualToString('picker._selectElement.value', '1');
-    shouldBeEqualToString('menuElement.value', 'foo');
+    shouldBeEqualToString('menu.value', 'foo');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'bar');
 
     // Escape should close the popup and select that keyboard selected item.
     eventSender.keyDown('escape');
     shouldBeNull('window.internals.pagePopupWindow');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'bar');
 
     waitUntilClosing(function() {
         openPicker(menu, test3, openPickerErrorCallback);
@@ -78,15 +88,18 @@ function test2() {
 function test3() {
     picker = window.internals.pagePopupWindow.global.picker;
     shouldBeEqualToString('picker._selectElement.value', '1');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'bar');
 
     eventSender.keyDown('g');
     shouldBeEqualToString('picker._selectElement.value', '4');
-    shouldBeEqualToString('menuElement.value', 'bar');
+    shouldBeEqualToString('menu.value', 'bar');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     // Escape should close the popup and select that keyboard selected item.
     eventSender.keyDown('escape');
     shouldBeNull('window.internals.pagePopupWindow');
-    shouldBeEqualToString('menuElement.value', 'garply');
+    shouldBeEqualToString('menu.value', 'garply');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     waitUntilClosing(function() {
         openPicker(menu, test4, openPickerErrorCallback);
@@ -96,15 +109,18 @@ function test3() {
 function test4() {
     picker = window.internals.pagePopupWindow.global.picker;
     shouldBeEqualToString('picker._selectElement.value', '4');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     hoverOverElement(picker._selectElement.children[0]);
     shouldBeEqualToString('picker._selectElement.value', '0');
-    shouldBeEqualToString('menuElement.value', 'garply');
+    shouldBeEqualToString('menu.value', 'garply');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     // Escape should close the popup. Simply hovering over an item should not select it.
     eventSender.keyDown('escape');
     shouldBeNull('window.internals.pagePopupWindow');
-    shouldBeEqualToString('menuElement.value', 'garply');
+    shouldBeEqualToString('menu.value', 'garply');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     waitUntilClosing(function() {
         openPicker(menu, test5, openPickerErrorCallback);
@@ -114,19 +130,43 @@ function test4() {
 function test5() {
     picker = window.internals.pagePopupWindow.global.picker;
     shouldBeEqualToString('picker._selectElement.value', '4');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     hoverOverElement(picker._selectElement.children[0]);
     shouldBeEqualToString('picker._selectElement.value', '0');
-    shouldBeEqualToString('menuElement.value', 'garply');
+    shouldBeEqualToString('menu.value', 'garply');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'garply');
 
     eventSender.keyDown('leftArrow');
     shouldBeEqualToString('picker._selectElement.value', '0');
-    shouldBeEqualToString('menuElement.value', 'garply');
+    shouldBeEqualToString('menu.value', 'garply');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
 
     // But if you press any key, the highlighted item should be selected.
     eventSender.keyDown('escape');
     shouldBeNull('window.internals.pagePopupWindow');
-    shouldBeEqualToString('menuElement.value', 'foo');
+    shouldBeEqualToString('menu.value', 'foo');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
+
+    waitUntilClosing(function() {
+        openPicker(menu, test6, openPickerErrorCallback);
+    });
+}
+
+function test6() {
+    picker = window.internals.pagePopupWindow.global.picker;
+    shouldBeEqualToString('picker._selectElement.value', '0');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
+
+    eventSender.keyDown('downArrow');
+    shouldBeEqualToString('picker._selectElement.value', '1');
+    shouldBeEqualToString('menu.value', 'foo');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'bar');
+
+    clickElement(picker._selectElement.children[0]);
+    shouldBeNull('window.internals.pagePopupWindow');
+    shouldBeEqualToString('menu.value', 'foo');
+    shouldBeEqualToString('internals.selectMenuListText(menu)', 'foo');
 
     finishJSTest();
 }

LayoutTests/platform/mac/fast/forms/select/menulist-popup-type-ahead-style-change-expected.txt
@@ -1,10 +1,7 @@
+FAIL picker didn't open
+PASS successfullyParsed is true
+
+TEST COMPLETE
 Tests <select> style change when popup is visible after selection is changed by keyboard typeahead. The selection should not change when style changes. Needs run-layout-test.
 
-layer at (0,0) size 39x19 backgroundClip at (8,50) size 39x19 clip at (8,50) size 39x19 outlineClip at (8,50) size 39x19
-  LayoutBlockFlow (positioned) {DIV} at (8,50) size 39x19
-    LayoutMenuList {SELECT} at (0,1) size 39x18 [bgcolor=#FFFF00] [border: (1px solid #A6A6A6)]
-      LayoutBlockFlow (anonymous) at (1,1) size 37x16
-        LayoutText at (8,1) size 8x13
-          text run at (8,1) width 8: "C"
-    LayoutText {#text} at (0,0) size 0x0
 

LayoutTests/platform/win/fast/forms/select/popup-menu-key-operations-expected.txt
@@ -1,39 +1,67 @@
 PASS picker._selectElement.value is "1"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "bar"
 PASS picker._selectElement.value is "4"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "4"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "1"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "bar"
 PASS picker._selectElement.value is "0"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "foo"
 PASS picker._selectElement.value is "0"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "foo"
 PASS window.internals.pagePopupWindow is null
-PASS menuElement.value is "foo"
+PASS menu.value is "foo"
+PASS internals.selectMenuListText(menu) is "foo"
 PASS picker._selectElement.value is "0"
+PASS internals.selectMenuListText(menu) is "foo"
 PASS picker._selectElement.value is "1"
-PASS menuElement.value is "foo"
+PASS menu.value is "foo"
+PASS internals.selectMenuListText(menu) is "bar"
 PASS window.internals.pagePopupWindow is null
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "bar"
 PASS picker._selectElement.value is "1"
+PASS internals.selectMenuListText(menu) is "bar"
 PASS picker._selectElement.value is "4"
-PASS menuElement.value is "bar"
+PASS menu.value is "bar"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS window.internals.pagePopupWindow is null
-PASS menuElement.value is "garply"
+PASS menu.value is "garply"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "4"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "0"
-PASS menuElement.value is "garply"
+PASS menu.value is "garply"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS window.internals.pagePopupWindow is null
-PASS menuElement.value is "garply"
+PASS menu.value is "garply"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "4"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "0"
-PASS menuElement.value is "garply"
+PASS menu.value is "garply"
+PASS internals.selectMenuListText(menu) is "garply"
 PASS picker._selectElement.value is "0"
-PASS menuElement.value is "garply"
+PASS menu.value is "garply"
+PASS internals.selectMenuListText(menu) is "foo"
 PASS window.internals.pagePopupWindow is null
-PASS menuElement.value is "foo"
+PASS menu.value is "foo"
+PASS internals.selectMenuListText(menu) is "foo"
+PASS picker._selectElement.value is "0"
+PASS internals.selectMenuListText(menu) is "foo"
+PASS picker._selectElement.value is "1"
+PASS menu.value is "foo"
+PASS internals.selectMenuListText(menu) is "bar"
+PASS window.internals.pagePopupWindow is null
+PASS menu.value is "foo"
+PASS internals.selectMenuListText(menu) is "foo"
 PASS successfullyParsed is true
 
 TEST COMPLETE

Source/core/testing/Internals.cpp
@@ -1986,6 +1986,17 @@ String Internals::getImageSourceURL(Element* element)
     return element->imageSourceURL();
 }
 
+String Internals::selectMenuListText(HTMLSelectElement* select)
+{
+    ASSERT(select);
+    LayoutObject* renderer = select->layoutObject();
+    if (!renderer || !renderer->isMenuList())
+        return String();
+
+    LayoutMenuList* menuList = toLayoutMenuList(renderer);
+    return menuList->text();
+}
+
 bool Internals::isSelectPopupVisible(Node* node)
 {
     ASSERT(node);

Source/core/testing/Internals.h
@@ -285,6 +285,7 @@ public:
 
     String getImageSourceURL(Element*);
 
+    String selectMenuListText(HTMLSelectElement*);
     bool isSelectPopupVisible(Node*);
     bool selectPopupItemStyleIsRtl(Node*, int);
     int selectPopupItemStyleFontHeight(Node*, int);

Source/core/testing/Internals.idl
@@ -244,6 +244,7 @@
 
     [TypeChecking=Interface] DOMString getImageSourceURL(Element element);
 
+    [TypeChecking=Interface] DOMString selectMenuListText(HTMLSelectElement select);
     [TypeChecking=Interface] boolean isSelectPopupVisible(Node node);
     boolean selectPopupItemStyleIsRtl(Node select, long itemIndex);
     long selectPopupItemStyleFontHeight(Node select, long itemIndex);