da18e6c3b4be2fc5a13b304820a24dfe71b39d09
rtoy@google.com <rtoy@google.com>
2015-04-22 23:09:05 +0000

    Merge 194069 "Display a warning message on CORS access restricti..."
    > Display a warning message on CORS access restriction MediaElementAudioSource
    > 
    > BUG=465231
    > TEST=Covered by media-element-audio-source-node-cross-origin
    > 
    > Review URL: https://codereview.chromium.org/1073663002
    
    TBR=rtoy@chromium.org
    
    Review URL: https://codereview.chromium.org/1086263004
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2357@194246 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/modules/webaudio/MediaElementAudioSourceNode.h
+++ Source/modules/webaudio/MediaElementAudioSourceNode.h
@@ -53,6 +53,7 @@ public:
 
     // AudioSourceProviderClient
     virtual void setFormat(size_t numberOfChannels, float sampleRate) override;
+    void onCurrentSrcChanged(const KURL& currentSrc);
 
     virtual void lock() override;
     virtual void unlock() override;
@@ -63,15 +64,40 @@ private:
     // As an audio source, we will never propagate silence.
     virtual bool propagatesSilence() const override { return false; }
 
+    // Must be called only on the audio thread.
     bool passesCORSAccessCheck();
 
-    RefPtrWillBeMember<HTMLMediaElement> m_mediaElement;
+    // Must be called only on the main thread.
+    bool passesCurrentSrcCORSAccessCheck(const KURL& currentSrc);
+
+    // Print warning if CORS restrictions cause MediaElementAudioSource to output zeroes.
+    void printCORSMessage();
+
+    // This Persistent doesn't make a reference cycle. The reference from
+    // HTMLMediaElement to AudioSourceProvideClient, which
+    // MediaElementAudioSourceNode implements, is weak.
+    RefPtrWillBePersistent<HTMLMediaElement> m_mediaElement;
     Mutex m_processLock;
 
     unsigned m_sourceNumberOfChannels;
     double m_sourceSampleRate;
 
     OwnPtr<MultiChannelResampler> m_multiChannelResampler;
+
+    // |m_passesCurrentSrcCORSAccessCheck| holds the value of
+    // context()->securityOrigin() && context()->securityOrigin()->canRequest(mediaElement()->currentSrc()),
+    // updated in the ctor and onCurrentSrcChanged() on the main thread and
+    // used in passesCORSAccessCheck() on the audio thread,
+    // protected by |m_processLock|.
+    bool m_passesCurrentSrcCORSAccessCheck;
+
+    // Indicates if we need to print a CORS message if the current source has changed and we have no
+    // access to it. Must be protected by |m_processLock|.
+    bool m_maybePrintCORSMessage;
+
+    // The value of mediaElement()->currentSrc() in the ctor and onCurrentSrcChanged().  Protected
+    // by |m_processLock|.
+    KURL m_currentSrc;
 };
 
 class MediaElementAudioSourceNode final : public AudioSourceNode {
