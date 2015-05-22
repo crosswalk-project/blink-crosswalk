de5732d620f1c45434a34f47790c4b3c013425d0
timvolodine@chromium.org <timvolodine@chromium.org>
2015-05-22 15:29:09 +0000

    Add WebRuntimeFeatures::enablePermissionsAPI.
    Add the ability to control whether we want to expose the Permissions API
    in blink at runtime. This is e.g. needed for WebView where the
    Permissions API is currently not supported.
    
    BUG=489926
    TBR=dglazkov
    
    Review URL: https://codereview.chromium.org/1150653002
    
    (cherry picked from commit 867b90077485d07fc4dc60de24112bdad2085e6a)
    
    Review URL: https://codereview.chromium.org/1150163003
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2403@195789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/web/WebRuntimeFeatures.cpp
+++ Source/web/WebRuntimeFeatures.cpp
@@ -193,6 +193,11 @@ void WebRuntimeFeatures::enablePagePopup(bool enable)
     RuntimeEnabledFeatures::setPagePopupEnabled(enable);
 }
 
+void WebRuntimeFeatures::enablePermissionsAPI(bool enable)
+{
+    RuntimeEnabledFeatures::setPermissionsEnabled(enable);
+}
+
 void WebRuntimeFeatures::enableRequestAutocomplete(bool enable)
 {
     RuntimeEnabledFeatures::setRequestAutocompleteEnabled(enable);
--- public/web/WebRuntimeFeatures.h
+++ public/web/WebRuntimeFeatures.h
@@ -101,6 +101,8 @@ public:
 
     BLINK_EXPORT static void enablePagePopup(bool);
 
+    BLINK_EXPORT static void enablePermissionsAPI(bool);
+
     BLINK_EXPORT static void enableRequestAutocomplete(bool);
 
     BLINK_EXPORT static void enableScreenOrientation(bool);
