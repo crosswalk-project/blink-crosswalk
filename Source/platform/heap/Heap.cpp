004aaf8e3f32218d60ed004d0fadca9469195cae
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-04 12:43:59 +0000

    Merge 195758 "Oilpan: complete current sweep before preparing fo..."
    > Oilpan: complete current sweep before preparing for new GC.
    > 
    > If collectGarbage() is called while sweeping is in progress, complete
    > the sweeping before recording what GCState to revert back to should
    > we fail to get the other threads to stop at a safe point.
    > 
    > Otherwise we risk reverting back to a sweeping state that has been
    > completed & advanced past.
    > 
    > R=haraken
    > BUG=487952
    > 
    > Review URL: https://codereview.chromium.org/1153533002
    
    TBR=sigbjornf@opera.com
    
    Review URL: https://codereview.chromium.org/1154243005
    
    git-svn-id: svn://svn.chromium.org/blink/branches/chromium/2403@196494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- Source/platform/heap/Heap.cpp
+++ Source/platform/heap/Heap.cpp
@@ -2267,6 +2267,7 @@ void Heap::collectGarbage(ThreadState::StackState stackState, ThreadState::GCTyp
 {
     ThreadState* state = ThreadState::current();
     RELEASE_ASSERT(!state->isInGC());
+    state->completeSweep();
     ThreadState::GCState originalGCState = state->gcState();
     state->setGCState(ThreadState::StoppingOtherThreads);
 
